862fec5e29d0edb9b3a47cd805bf09d811bbb8d1
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-14 21:46:47 +0900

    feat(eslint,prettier): install lint packages

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
     "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
-    "pre-commit": "lint-staged -d -v",
+    "pre-commit": "eslint . && lint-staged -d -v",
   },
 };
