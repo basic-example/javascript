83c5fee326af35890f644eb02ba24cf451aa2750
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-14 13:06:22 +0900

    feat(angular12): universal example

---
+++ angular/angular12-universal/src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule.withServerTransition({ appId: "serverApp" })],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
