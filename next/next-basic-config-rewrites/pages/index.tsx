dca91ce7a4fdc47835e59f54c63fba518af2898b
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-17 15:50:29 +0900

    feat(next): basic-config-rewrites example

---
+++ next/next-basic-config-rewrites/pages/index.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+export default function RootPage(): JSX.Element {
+  return (
+    <div>
+      <h1>Root Page</h1>
+      <p>
+        recommand open in new window to show valid url change result instead of
+        codesandbox browser.
+      </p>
+      <Link href="/home">home (rewrite Main)</Link>
+    </div>
+  );
+}
