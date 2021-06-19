4fef3b16723cefcedba3158ee458eeba57a99643
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-19 19:00:12 +0900

    feat(next): basic-500-page example

---
+++ next/next-basic-500-page/pages/500.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Custom500Page(): JSX.Element {
+  return (
+    <div>
+      <h1>500 Page</h1>
+    </div>
+  );
+}
+
+// 500 page can't have getServerSideProps
