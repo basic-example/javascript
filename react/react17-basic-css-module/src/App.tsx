8ae89de6974e847b4e8037b085223a1c7a4f3cd8
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-25 18:09:38 +0900

    feat(react17): basic css module example

---
+++ react/react17-basic-css-module/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./style.module.css";
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1 className={styles.title}>App</h1>
+      <h2 className="title">Hello World</h2>
+      {/* converted like class="_src_style_module__title" */}
+    </div>
+  );
+};
+
+export default App;
