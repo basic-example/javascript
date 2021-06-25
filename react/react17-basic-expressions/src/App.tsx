902d3be1a563129e49719f0bf916af41f52167da
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-26 06:47:20 +0900

    feat(react17): basic expression example

---
+++ react/react17-basic-expressions/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = (): JSX.Element => {
+  const name = "World";
+
+  return (
+    <div>
+      <h1>App</h1>
+      <h2>Hello {name}</h2>
+    </div>
+  );
+};
+
+export default App;
