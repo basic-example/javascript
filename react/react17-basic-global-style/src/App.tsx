09e011dbe786008c65ce2f5712b6f24d4cf00851
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-25 18:43:00 +0900

    feat(react17): basic global style example

---
+++ react/react17-basic-global-style/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1>App</h1>
+      <h2 className="title">Hello World</h2>
+    </div>
+  );
+};
+
+export default App;
