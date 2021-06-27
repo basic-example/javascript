90c25f0d918b077bdc8da3f08d015517515d300b
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-27 20:55:26 +0900

    feat(vue3): basic reactive api readonly example

---
+++ vue/vue3-basic-reactive-api-readonly/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>App Component</h1>
+  <textarea
+    style="width: 100%; height: 15em"
+    disabled
+    value="
+      const obj = {
+        count: 1,
+      };
+
+      ++reactive(obj).count; // success;
+
+      const reactivedCountIncreased = obj.count == 2;
+
+      ++readonly(reactive(obj)).count; // fail
+      ++readonly(obj).count; // fail
+
+      const readonlyedCountIncreased = obj.count > 2;
+    "
+  />
+</template>
+
+<script>
+import { defineComponent, reactive, readonly } from "vue";
+import log from "./utils/log";
+
+const obj = {
+  count: 1,
+};
+
+++reactive(obj).count; // success;
+
+const reactivedCountIncreased = obj.count == 2;
+
+++readonly(reactive(obj)).count; // fail
+++readonly(obj).count; // fail
+
+const readonlyedCountIncreased = obj.count > 2;
+
+log("reactivedCountIncreased", reactivedCountIncreased);
+log("readonlyedCountIncreased", readonlyedCountIncreased);
+
+export default defineComponent({});
+</script>
+
+<style></style>
