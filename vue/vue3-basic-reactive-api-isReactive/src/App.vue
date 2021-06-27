4e8c486bcff608b467a9c185f08d6328158e1c74
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-27 14:07:51 +0900

    feat(vue3): basic reactive api isReactive example

---
+++ vue/vue3-basic-reactive-api-isReactive/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>App Component</h1>
+  <textarea
+    style="width: 100%; height: 8em"
+    disabled
+    value="
+      const obj = {
+        count: 1,
+      };
+      const readonlyed = readonly(obj);
+      const reactived = reactive(obj);
+    "
+  />
+</template>
+
+<script>
+import { defineComponent, isReactive, reactive, readonly } from "vue";
+import log from "./utils/log";
+
+const obj = {
+  count: 1,
+};
+const readonlyed = readonly(obj);
+const reactived = reactive(obj);
+
+log("isReactive(readonlyed)", isReactive(readonlyed));
+log("isReactive(reactived)", isReactive(reactived));
+log("isReactive(readonly(reactived))", isReactive(readonly(reactived)));
+log("isReactive(reactive(reactived))", isReactive(reactive(reactived)));
+log("isReactive(readonly(readonlyed))", isReactive(readonly(readonlyed)));
+log("isReactive(reactive(readonlyed))", isReactive(reactive(readonlyed)));
+
+export default defineComponent({});
+</script>
+
+<style></style>
