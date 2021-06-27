a850af08fb8a10c4c272ea0a7814eb4466f25bb8
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-27 13:24:09 +0900

    feat(vue3): basic reactive api isProxy example

---
+++ vue/vue3-basic-reactive-api-isProxy/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>App Component</h1>
+  <textarea
+    style="width: 100%; height: 12em"
+    disabled
+    value="
+      const obj = {
+        count: 1,
+      };
+      const readonlyed = readonly(obj);
+      const reactived = reactive(obj);
+    "
+  />
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, isProxy, reactive, readonly } from "vue";
+import log from "./utils/log";
+
+const obj = {
+  count: 1,
+};
+const readonlyed = readonly(obj);
+const reactived = reactive(obj);
+
+log("isProxy(readonlyed)", isProxy(readonlyed));
+log("isProxy(reactived)", isProxy(reactived));
+
+export default defineComponent({});
+</script>
+
+<style></style>
