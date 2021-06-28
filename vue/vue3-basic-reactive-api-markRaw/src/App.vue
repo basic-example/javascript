eac492ac618473a5aaf546ce9760b8ae9cfc17ee
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-28 22:37:03 +0900

    feat(vue3): basic reactive api markRaw example

---
+++ vue/vue3-basic-reactive-api-markRaw/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, isProxy, isReactive, markRaw, reactive } from "vue";
+import log from "./utils/log";
+
+const obj = {
+  count: 1,
+};
+
+const raw = markRaw(obj);
+
+log("obj", obj); // {count:1, __v_skip: true}
+log("raw", raw); // {count:1, __v_skip: true}
+log("raw === obj", raw === obj);
+log("isProxy(raw)", isProxy(raw));
+log("isReactive(reactive(raw))", isReactive(reactive(raw)));
+
+export default defineComponent({});
+</script>
+
+<style></style>
