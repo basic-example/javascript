6133eb71bccc4555fef4e65f8f7330045b68d103
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-28 10:14:02 +0900

    feat(vue3): basic reactive api toRaw example

---
+++ vue/vue3-basic-reactive-api-toRaw/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <textarea
+      style="width: 100%; height: 7em"
+      disabled
+      value="
+        const obj = {
+          count: 1,
+        };
+        const reactived = reactive(obj);
+      "
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, isReactive, reactive, toRaw } from "vue";
+import log from "./utils/log";
+
+const obj = {
+  count: 1,
+};
+const reactived = reactive(obj);
+
+log("obj", obj);
+log("reactived", reactived);
+log("toRaw(reactived)", toRaw(reactived));
+log("isReactive(reactived)", isReactive(reactived));
+log("isReactive(toRaw(reactived))", isReactive(toRaw(reactived)));
+log("reactived === obj", reactived === obj);
+log("toRaw(reactived) === obj", toRaw(reactived) === obj);
+
+export default defineComponent({});
+</script>
+
+<style></style>
