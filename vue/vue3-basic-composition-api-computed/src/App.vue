a72b9b875ca0bf89a8fb11d84d615b44a6739859
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-30 08:56:35 +0900

    feat(vue3): basic composition api computed example

---
+++ vue/vue3-basic-composition-api-computed/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>App Component</h1>
+  <p>count: {{ count }}</p>
+  <p>multiple count: {{ multipleCount }}</p>
+  <button @click="increaseCount">increaseCount</button>
+  <button @click="setCount">set count</button>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from "vue";
+
+const count = ref(1);
+const multipleCount = computed(() => {
+  console.log("computed run");
+  return 2 * count.value;
+});
+const increaseCount = () => {
+  count.value++;
+};
+const setCount = () => {
+  count.value = 10;
+};
+
+export default defineComponent({
+  setup() {
+    return {
+      count,
+      increaseCount,
+      multipleCount,
+      setCount,
+    };
+  },
+});
+</script>
+
+<style></style>
