5ac7f0736d5318e31105336ee83b10502b1e9911
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-30 10:24:19 +0900

    feat(vue3): basic composition api reactive example

---
+++ vue/vue3-basic-composition-api-reactive/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>App Component</h1>
+  <p>Count: {{ data.count }}</p>
+  <button @click="increase">increase button</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+  setup() {
+    let data = reactive({ count: 0 });
+
+    return {
+      data,
+      increase: () => {
+        data.count++;
+      },
+    };
+  },
+});
+</script>
+
+<style></style>
