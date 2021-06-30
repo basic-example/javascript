2a88c8f4ccd6f852d6d4112848e921e197ad8521
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-01 08:02:46 +0900

    feat(vue3): basic directive v-is example

---
+++ vue/vue3-basic-directive-v-is/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <component :is="cmp" abcd="ffff" />
+    <!-- like vue2 :is attributes -->
+    <div v-is="cmp" abcd="ffff"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Child from "./Child.vue";
+
+export default defineComponent({
+  data: () => ({
+    cmp: Child,
+  }),
+});
+</script>
+
+<style></style>
---
+++ vue/vue3-basic-directive-v-is/src/Child.vue
@@ -0,0 +1,13 @@
+<template>
+  <p a="aaa" b="bbb" c="ccc">
+    <span>host element is replace and props is merged</span>
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style></style>
