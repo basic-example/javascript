581d2e4efb1d7f6240bece642afe799072e736a7
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-01 07:45:42 +0900

    feat(vue3): basic component property $slot example

---
+++ vue/vue3-basic-component-property-$slot/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <Example>
+      <template #header>
+        <h2>header content</h2>
+      </template>
+      <template #default="{ user: person }">
+        <p>main {{ person }} content</p>
+      </template>
+      <template #footer>
+        <p>footer content</p>
+      </template>
+    </Example>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Example from "./Example.vue";
+
+export default defineComponent({
+  components: {
+    Example,
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue3-basic-component-property-$slot/src/Example.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { Slot, defineComponent, h } from "vue";
+
+export default defineComponent({
+  render() {
+    const header = this.$slots.header;
+    const body = this.$slots.default;
+    const footer = this.$slots.footer;
+
+    return h("div", [
+      (header as Slot)(),
+      (body as Slot)({ user: "Lee" }),
+      (footer as Slot)(),
+    ]);
+  },
+});
+</script>
+
+<style></style>
