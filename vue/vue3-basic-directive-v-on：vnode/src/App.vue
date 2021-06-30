ee1e2dff960968e54685e3f7a703f8ad4b892836
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-01 08:24:42 +0900

    feat(vue3): basic directive v-on:vnode example

---
+++ vue/vue3-basic-directive-v-on：vnode/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <button @click="show = !show">show toggle</button>
+    <Child
+      v-if="show"
+      @vnode-beforeCreate="beforeCreate"
+      @vnode-created="created"
+      @vnode-beforeMount="beforeMount"
+      @vnode-mounted="mounted"
+      @vnode-beforeUpdate="beforeUpdate"
+      @vnode-updated="updated"
+      @vnode-beforeUnmount="beforeUnmount"
+      @vnode-unmount="unmount"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Child from "./Child.vue";
+
+export default defineComponent({
+  components: {
+    Child,
+  },
+  data() {
+    return {
+      show: true,
+    };
+  },
+  methods: {
+    beforeCreate() {
+      console.log("child vnode-beforeCreate");
+    },
+    beforeMount() {
+      console.log("child vnode-beforeMount");
+    },
+    beforeUnmount() {
+      console.log("child vnode-beforeUnmount");
+    },
+    beforeUpdate() {
+      console.log("child vnode-beforeUpdate");
+    },
+    created() {
+      console.log("child vnode-created");
+    },
+    mounted() {
+      console.log("child vnode-mounted");
+    },
+    unmount() {
+      console.log("child vnode-unmount");
+    },
+    updated() {
+      console.log("child vnode-updated");
+    },
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue3-basic-directive-v-on：vnode/src/Child.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p>count : {{ count }}</p>
+    <button @click="count++">increase count</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+/* eslint-disable vue/order-in-components */
+export default defineComponent({
+  beforeCreate() {
+    console.log("child beforeCreate");
+  },
+  beforeMount() {
+    console.log("child beforeMount");
+  },
+  beforeUnmount() {
+    console.log("child beforeUnmount");
+  },
+  beforeUpdate() {
+    console.log("child beforeUpdate");
+  },
+  created() {
+    console.log("child created");
+  },
+  data() {
+    return {
+      count: 1,
+    };
+  },
+  mounted() {
+    console.log("child mounted");
+  },
+  unmount() {
+    console.log("child unmount");
+  },
+  updated() {
+    console.log("child updated");
+  },
+});
+/* eslint-enable vue/order-in-components */
+</script>
+
+<style></style>
