2a61ff5bac83939879b7f2a64b776efcea9a2ef4
jongwoo Yoo <dbwhddn10@gmail.com>
2021-06-30 10:29:59 +0900

    feat(vue3): basic composition api ref example

---
+++ vue/vue3-basic-composition-api-ref/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>App Component</h1>
+  <p>Count: {{ count }}</p>
+  <button @click="increase">increase</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    let count = ref(0);
+
+    return {
+      count,
+      increase: () => {
+        count.value++;
+      },
+    };
+  },
+});
+</script>
+
+<style></style>
