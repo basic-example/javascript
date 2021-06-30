c32e160a50e6d92cdb7f2e79d04f7305132ee7a7
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-01 08:00:00 +0900

    feat(vue3): basic element template example

---
+++ vue/vue3-basic-element-template/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <template v-for="(item, index) in items" :key="index">
+      <p>{{ item }}</p>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      items: ["1th item", "2th item", "3th item"],
+    };
+  },
+});
+</script>
+
+<style></style>
