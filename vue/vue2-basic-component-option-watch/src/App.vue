a8e1e172acbd800180ac2bc68ee3812e82044c4b
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 07:54:32 +0900

    feat(vue2): basic component option watch example

---
+++ vue/vue2-basic-component-option-watch/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    id: <input v-model="id" type="number" />
+    <hr />
+    {{ title || "this message will be changed when id value changed" }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      id: 1,
+      title: null,
+    };
+  },
+  watch: {
+    id(next) {
+      fetch(`https://jsonplaceholder.typicode.com/todos/${next}`)
+        .then((response) => response.json())
+        .then((json) => {
+          this.$data.title = json.title;
+        });
+    },
+  },
+});
+</script>
+
+<style></style>
