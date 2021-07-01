f3a9d2d4970f044bf5cf7f079cc1dbcf4f906163
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 08:56:15 +0900

    feat(vue2): basic component option filters example

---
+++ vue/vue2-basic-component-option-filters/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <p>{{ "abcd" | capitalize }}</p>
+    <Child :title="'hello world!' | capitalize" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Child from "./Child.vue";
+
+export default Vue.extend({
+  components: {
+    Child,
+  },
+  filters: {
+    capitalize: (val: string) => val.toUpperCase(),
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-component-option-filters/src/Child.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    title: {
+      default: "",
+      type: String,
+    },
+  },
+});
+</script>
+
+<style></style>
