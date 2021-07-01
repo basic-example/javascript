32a968674e657dce2529ff695c7442003124f05e
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-01 19:07:56 +0900

    feat(vue2): basic component property

---
+++ vue/vue2-basic-component-property-$attrs/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <Parent a="aaa" b="bbb" c="ccc" d="ddd" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Parent from "./Parent.vue";
+
+export default Vue.extend({
+  components: {
+    Parent,
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-component-property-$attrs/src/Parent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Parent Component</h2>
+    <p>{{ $attrs }}</p>
+    <p v-bind="$attrs">check this element attributes</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    a: {
+      default: null,
+      type: String,
+    },
+    b: {
+      default: null,
+      type: String,
+    },
+  },
+});
+</script>
+
+<style></style>
