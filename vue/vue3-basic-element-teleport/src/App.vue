8caaf2736d200b2d1b80e21d2b9bc95c46209c1d
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-01 10:14:36 +0900

    feat(vue3): basic element teleport example

---
+++ vue/vue3-basic-element-teleport/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>App Component</h1>
+  <teleport to="body">
+    <h2>append child to body</h2>
+  </teleport>
+  <teleport to="#modal">
+    <h2>append child to #modal in public/index.htmnl</h2>
+  </teleport>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style></style>
