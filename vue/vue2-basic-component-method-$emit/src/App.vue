6bf72fca20e711d1399fc17ce462cf792df0830e
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-01 19:38:22 +0900

    feat(vue2): basic component method  example

---
+++ vue/vue2-basic-component-method-$emit/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <Child @custom-event="alertMessage($event)" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Child from "./Child.vue";
+
+export default Vue.extend({
+  components: {
+    Child,
+  },
+  methods: {
+    alertMessage: (event: string) => {
+      alert(event);
+    },
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-component-method-$emit/src/Child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <button @click="$emit('custom-event', 'message from child component')">
+      button
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style></style>
