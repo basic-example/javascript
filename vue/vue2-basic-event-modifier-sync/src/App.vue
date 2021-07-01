2c0d43fb71b4b97014aec77cdadbaa4f51a7d22a
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 06:18:06 +0900

    feat(vue2): basic event modifier sync example

---
+++ vue/vue2-basic-event-modifier-sync/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <p>count(App): {{ count }}</p>
+    <hr />
+    <Child :count="count" @update:count="count = $event" />
+    <p>above code is equivalent to</p>
+    <Child :count.sync="count" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Child from "./Child.vue";
+
+export default Vue.extend({
+  components: {
+    Child,
+  },
+  data: () => ({
+    count: 1,
+  }),
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-event-modifier-sync/src/Child.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p>count (child): {{ count }}</p>
+    <button @click="increase">increase</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    increase() {
+      this.$emit("update:count", this.count + 1);
+
+      // below code has warn ocurred
+      // this.count++;
+      // this.$emit("update:count", this.count + 1);
+    },
+  },
+  props: {
+    count: {
+      default: null,
+      type: Number,
+    },
+  },
+});
+</script>
+
+<style></style>
