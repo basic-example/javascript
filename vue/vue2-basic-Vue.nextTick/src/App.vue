8d6c774af4bcaa78a38e961c91e3d1d08d82d828
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 10:35:35 +0900

    feat(vue2): basic Vue.nextTick example

---
+++ vue/vue2-basic-Vue.nextTick/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <hr />
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      msg: "custom message",
+    };
+  },
+  mounted() {
+    this.$data.msg = "hello world";
+
+    console.log(this.$el.textContent); // custom message
+
+    Vue.nextTick(() => {
+      console.log(this.$el.textContent); // hello world
+    });
+
+    // below code is equivalent to previous code
+    // await this.$nextTick()
+    // console.log(this.$el.textContent);
+  },
+});
+</script>
+
+<style></style>
