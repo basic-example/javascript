cb74134952b05fcfe9c959761f5516a1e64f52e4
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-03 00:38:36 +0900

    feat(vue2): basic Vue.config.ignoredElements example

---
+++ vue/vue2-basic-Vue.config.ignoredElements/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <p>Hello elements is not ignored</p>
+    <Hello />
+    <p>World elements is ignored</p>
+    <World />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Hello from "./Hello.vue";
+
+export default Vue.extend({
+  components: {
+    Hello,
+  },
+});
+</script>
+
+<style></style>
