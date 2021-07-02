aa745cd2b9f52a0c549c18b9af02bec46effa5ff
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 15:46:37 +0900

    feat(vue2): basic component option components example

---
+++ vue/vue2-basic-component-option-components/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <CustomInput />
+    <custom-input />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import CustomInput from "./CustomInput.vue";
+
+export default Vue.extend({
+  components: {
+    CustomInput,
+  },
+});
+</script>
+
+<style></style>
