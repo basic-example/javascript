8766eb8d04a4cda82c23b7e25fc16db97db401cf
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 17:44:55 +0900

    feat(vue2): basic directive v-pre example

---
+++ vue/vue2-basic-directive-v-pre/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <Child>{{ message }}</Child>
+    <hr />
+    <!-- this will not be compiled -->
+    <div v-pre>{{ message }}</div>
+    <Child v-pre>{{ message }}</Child>
+    <div v-pre>
+      <Child>{{ message }}</Child>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Child from "./Child.vue";
+
+export default Vue.extend({
+  components: {
+    Child,
+  },
+  data() {
+    return {
+      message: "Hello World!",
+    };
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-directive-v-pre/src/Child.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style></style>
