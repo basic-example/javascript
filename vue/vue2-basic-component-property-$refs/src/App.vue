c202a5ee74ea17fd5363c4e5e3d2f0ad8e13a48f
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 13:34:16 +0900

    feat(vue2): basic component property  example

---
+++ vue/vue2-basic-component-property-$refs/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <CustomInput ref="customRef" />
+    <hr />
+    <button @click="customInputFocus">customInputFocus</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import CustomInput from "./CustomInput.vue";
+
+export default Vue.extend({
+  components: {
+    CustomInput,
+  },
+  methods: {
+    customInputFocus() {
+      (
+        (this as Vue).$refs.customRef as InstanceType<typeof CustomInput>
+      ).inputFocus();
+    },
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-component-property-$refs/src/CustomInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Custom Input Component</h2>
+    <input ref="abcd" type="text" />
+    <hr />
+    <button @click="inputFocus">inputFocus</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    inputFocus(): void {
+      ((this as Vue).$refs.abcd as HTMLInputElement).focus();
+    },
+  },
+});
+</script>
+
+<style></style>
