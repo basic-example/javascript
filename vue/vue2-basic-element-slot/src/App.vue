4478e2c49423148a94149c2dbb3b1af3330fc8f4
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 10:24:51 +0900

    feat(vue2): basic element slot example

---
+++ vue/vue2-basic-element-slot/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <Example>
+      <h2>Child Component Message</h2>
+    </Example>
+    <Example #default="{ user: person }">
+      <h2>default</h2>
+      <p>person: {{ person }}</p>
+    </Example>
+    <Example>
+      <template #[temp]="{ user: person1 }">
+        <h2>default</h2>
+        <p>person: {{ person1 }}</p>
+      </template>
+      <template #main="{ user: person2 }">
+        <h2>main</h2>
+        <p>person: {{ person2 }}</p>
+      </template>
+      <template #sub="{ user: person3 }">
+        <h2>sub</h2>
+        <p>person: {{ person3 ? true : "no value" }}</p>
+      </template>
+    </Example>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Example from "./Example.vue";
+
+export default Vue.extend({
+  components: {
+    Example,
+  },
+  data: () => ({
+    temp: "default",
+  }),
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-element-slot/src/Example.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot :user="user" />
+    <slot name="main" :user="user" />
+    <slot name="sub" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data: () => ({
+    user: "Lee",
+  }),
+});
+</script>
+
+<style></style>
