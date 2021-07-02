e407bfd1f61d1a5bf8a05bc934cdc4007fd935cd
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 16:03:54 +0900

    feat(vue2): basic directive v-model example

---
+++ vue/vue2-basic-directive-v-model/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <p>{{ message }}</p>
+    <input v-model="message" />
+    <input v-model="message" />
+    <input :value="message" @input="mesage = $event.target.value" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    message: "inital message",
+  }),
+});
+</script>
+
+<style></style>
