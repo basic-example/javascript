20eeef524a4fb2ff6b693b37f1cceb4e444872c7
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 11:23:16 +0900

    feat(vue2): basic style scoped example

---
+++ vue/vue2-basic-style-scoped/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <NonScoped />
+    <Scoped />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import NonScoped from "./NonScoped.vue";
+import Scoped from "./Scoped.vue";
+
+export default Vue.extend({
+  components: {
+    NonScoped,
+    Scoped,
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-style-scoped/src/NonScoped.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Scoped Component</h2>
+    <p>Scoped Component content</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+h2 {
+  color: blue;
+}
+</style>
