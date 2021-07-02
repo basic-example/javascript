9b642a04a36d0ed4fa31bfe61bc0f5e127eb3dc3
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 16:43:46 +0900

    feat(vue2): basic directive v-if example

---
+++ vue/vue2-basic-directive-v-if/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <p v-if="shown">show me</p>
+    <p v-if="hidden">hide me</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    hidden: false,
+    shown: true,
+  }),
+});
+</script>
+
+<style></style>
