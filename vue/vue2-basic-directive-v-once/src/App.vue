10a70024a4785133631c9ac51a85916f66c80bc8
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 12:38:49 +0900

    feat(vue2): basic directive v-once example

---
+++ vue/vue2-basic-directive-v-once/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <p>count : {{ count }}</p>
+    <button @click="increase">increase</button>
+    <hr />
+    <Child :count="count" />
+    <!-- not re-render -->
+    <Child v-once :count="count" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Child from "./Child.vue";
+
+export default Vue.extend({
+  components: {
+    Child,
+  },
+  data: () => ({
+    count: 1,
+  }),
+  methods: {
+    increase() {
+      ++this.count;
+    },
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-directive-v-once/src/Child.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p>count : {{ count }}</p>
+    <p>msg : {{ msg }}</p>
+    <button @click="addMark">addMark</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    msg: "hello world",
+  }),
+  methods: {
+    addMark() {
+      this.msg = this.msg + ".";
+    },
+  },
+  props: {
+    count: {
+      default: null,
+      type: Number,
+    },
+  },
+});
+</script>
+
+<style></style>
