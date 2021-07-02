6996d7253d8ede3aecf4f6ad568d4ab9b309ccbc
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 10:51:02 +0900

    feat(vue2): basic event modifier native example

---
+++ vue/vue2-basic-event-modifier-native/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <button @click="clicked">app button</button>
+    <hr />
+    <p>basic</p>
+    <CustomButton />
+    <hr />
+    <p>@click="clicked" @click2="clicked2" :trigger-emit="false"</p>
+    <CustomButton :trigger-emit="false" @click="clicked" @click2="clicked2" />
+    <hr />
+    <p>@click.native="clicked" @click2="clicked2" :trigger-emit="false"</p>
+    <CustomButton
+      :trigger-emit="false"
+      @click.native="clicked"
+      @click2="clicked2"
+    />
+    <hr />
+    <p>@click="clicked" @click2="clicked2" :trigger-emit="true"</p>
+    <CustomButton :trigger-emit="true" @click="clicked" @click2="clicked2" />
+    <hr />
+    <p>@click.native="clicked" @click2="clicked2" :trigger-emit="true"</p>
+    <CustomButton
+      :trigger-emit="true"
+      @click.native="clicked"
+      @click2="clicked2"
+    />
+    <p>
+      conclusion: dom native event should add native modifier when using parent
+      component. otherwise it should not trigger callback despite of definiton.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import CustomButton from "./CustomButton.vue";
+
+export default Vue.extend({
+  components: {
+    CustomButton,
+  },
+  methods: {
+    clicked() {
+      console.log("clicked in App");
+    },
+    clicked2() {
+      console.log("clicked2 in App");
+    },
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-event-modifier-native/src/CustomButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>CustomButton &lt;&lt; click for check</h2>
+    <button @click="customClick">button</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    customClick() {
+      console.log("clicked in CustomButton");
+      if (this.triggerEmit) {
+        this.$emit("click");
+        this.$emit("click2");
+      }
+    },
+  },
+  props: {
+    triggerEmit: {
+      type: Boolean,
+    },
+  },
+});
+</script>
+
+<style></style>
