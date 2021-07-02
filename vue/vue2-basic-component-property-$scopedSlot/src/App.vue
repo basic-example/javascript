6aae2c7f51a8eb64fba84a4dc2117158d8598ddd
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 09:35:06 +0900

    feat(vue2): basic component property $scopedSlots example

---
+++ vue/vue2-basic-component-property-$scopedSlot/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <Example>
+      <template #main="{ user: person }">
+        <p>username: {{ person }}</p>
+      </template>
+    </Example>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-component-property-$scopedSlot/src/Example.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import Vue from "vue";
+import { NormalizedScopedSlot } from "vue/types/vnode";
+
+export default Vue.extend({
+  data() {
+    return {
+      me: "Yoo",
+    };
+  },
+  render(h) {
+    return h("div", {}, [
+      (this.$scopedSlots as Record<string, NormalizedScopedSlot>).main({
+        user: this.$data.me,
+      }),
+    ]);
+  },
+});
+</script>
+
+<style></style>
