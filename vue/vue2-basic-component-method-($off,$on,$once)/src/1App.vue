b703c07f08c395faac515a4eb7645329ea19ab7f
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-03 08:53:59 +0900

    feat(vue2): basic component method ,, example

---
+++ vue/vue2-basic-component-method-($off,$on,$once)/src/1App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <Child @click="clickEvent" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Child from "./Child.vue";
+
+export default Vue.extend({
+  components: { Child },
+  methods: {
+    clickEvent() {
+      alert("clickEvent!");
+    },
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-component-method-($off,$on,$once)/src/Child.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <button @click="turnOff">turn-off click event</button>
+    <button @click="emit">emit click event</button>
+    <button @click="turnOn">turn-on click event</button>
+    <button @click="turnOnce">turn-once click event</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    emit() {
+      this.$emit("click");
+    },
+    turnOff() {
+      this.$off("click");
+    },
+    turnOn() {
+      this.$on("click", this.$listeners.click as typeof Function.prototype);
+    },
+    turnOnce() {
+      this.$once("click", this.$listeners.click as typeof Function.prototype);
+    },
+  },
+});
+</script>
+
+<style></style>
