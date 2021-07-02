d444f52efd1412219353646ac2cc2ebb66fcfdb7
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 09:00:20 +0900

    feat(vue2): basic component option mixins example

---
+++ vue/vue2-basic-component-option-mixins/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+const mixin = {
+  created() {
+    alert(`mixin created hook event is fired!`);
+  },
+};
+
+export default Vue.extend({
+  mixins: [mixin],
+});
+</script>
+
+<style></style>
