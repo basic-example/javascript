ae73fe0de852f432031b1fa182a36852fcf40d1c
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-02 11:13:07 +0900

    feat(vue2): basic component property  example

---
+++ vue/vue2-basic-component-property-$listeners/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <CustomInput @focus="focused" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import CustomInput from "./CustomInput.vue";
+
+export default Vue.extend({
+  components: {
+    CustomInput,
+  },
+  methods: {
+    focused: () => {
+      console.log("focus event fired");
+    },
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-component-property-$listeners/src/CustomInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Custom Input Component</h2>
+    <input type="text" v-on="{ ...$listeners, ...{ input: inputted } }" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    inputted: () => {
+      console.log("input event fired");
+    },
+  },
+});
+</script>
+
+<style></style>
