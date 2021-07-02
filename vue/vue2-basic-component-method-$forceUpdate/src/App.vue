8b8024e993dbaca73b5a5e7f48d9f80fb92e3151
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-03 03:23:25 +0900

    feat(vue2): basic component method  example

---
+++ vue/vue2-basic-component-method-$forceUpdate/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <div v-for="(num, i) in nums" :key="i">
+      {{ num }}
+    </div>
+    <button
+      @click="
+        () => {
+          pushNum();
+          $forceUpdate();
+        }
+      "
+    >
+      pushNum() && $forceUpdate();
+    </button>
+    <button
+      @click="
+        () => {
+          pushNum();
+        }
+      "
+    >
+      pushNum()
+    </button>
+    <button
+      @click="
+        () => {
+          newNums();
+        }
+      "
+    >
+      newNums()
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      nums: [1, 2, 3],
+    };
+  },
+  methods: {
+    newNums() {
+      this.nums = [Math.floor(Math.random() * 100) + 1];
+    },
+    pushNum() {
+      this.nums[this.nums.length] = Math.floor(Math.random() * 100) + 1;
+    },
+  },
+});
+</script>
+
+<style></style>
