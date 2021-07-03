d2a3070e62e5372aae73366daec2018b4f9de5cd
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-04 03:43:47 +0900

    feat(vue2): router RouteConfig name example

---
+++ vue/vue2-router-RouteConfig-name/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <router-link :to="{ name: 'aaa' }">aaa</router-link>
+    <router-link :to="{ name: 'bbb' }">bbb</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import VueRouter from "vue-router";
+import { RouteConfig } from "vue-router/types";
+
+const routes: RouteConfig[] = [
+  {
+    component: {
+      template: `<h2>A Component</h2>`,
+    },
+    name: "aaa",
+    path: "/a",
+  },
+  {
+    component: {
+      template: `<h2>B Component</h2>`,
+    },
+    name: "bbb",
+    path: "/b",
+  },
+];
+const router = new VueRouter({
+  routes,
+});
+
+export default Vue.extend({
+  router,
+});
+</script>
+
+<style>
+a {
+  display: block;
+}
+</style>
