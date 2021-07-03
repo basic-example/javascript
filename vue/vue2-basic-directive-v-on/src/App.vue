897d1e88ceacd10e5f4881409031a4bfb7b8a23f
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-03 09:06:02 +0900

    feat(vue2): basic directive v-on example

---
+++ vue/vue2-basic-directive-v-on/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <!-- eslint-disable-next-line vue/v-on-style -->
+    <button v-on:click="clicked">click button</button>
+    <button @click="clicked">click button</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  methods: {
+    clicked: function () {
+      alert("button is clicked~!");
+    },
+  },
+});
+</script>
+
+<style></style>
