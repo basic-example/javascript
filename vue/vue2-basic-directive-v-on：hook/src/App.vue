9c7520f75a35a88255ecb925e4a41727e01c2daf
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-03 11:39:08 +0900

    feat(vue2): basic directive v-on:hook example

---
+++ vue/vue2-basic-directive-v-on：hook/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <button @click="show = !show">show toggle</button>
+    <Child
+      v-if="show"
+      @hook:beforeCreate="beforeCreate"
+      @hook:created="created"
+      @hook:beforeMount="beforeMount"
+      @hook:mounted="mounted"
+      @hook:beforeUpdate="beforeUpdate"
+      @hook:updated="updated"
+      @hook:beforeDestroy="beforeDestroy"
+      @hook:destroyed="destroyed"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Child from "./Child.vue";
+
+export default Vue.extend({
+  components: {
+    Child,
+  },
+  data() {
+    return {
+      show: true,
+    };
+  },
+  methods: {
+    beforeCreate() {
+      console.log("child beforeCreate");
+    },
+    beforeDestroy() {
+      console.log("child beforedestroy");
+    },
+    beforeMount() {
+      console.log("child beforeMount");
+    },
+    beforeUpdate() {
+      console.log("child beforeUpdate");
+    },
+    created() {
+      console.log("child created");
+    },
+    destroyed() {
+      console.log("child destroyed");
+    },
+    mounted() {
+      console.log("child mounted");
+    },
+    updated() {
+      console.log("child updated");
+    },
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-directive-v-on：hook/src/Child.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p>count : {{ count }}</p>
+    <button @click="count++">increase count</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      count: 1,
+    };
+  },
+});
+</script>
+
+<style></style>
