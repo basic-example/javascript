f5dc4cfbabdd751355522daf88f34f9eceb4ae4b
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-03 12:35:09 +0900

    feat(vue2): basic component property  example

---
+++ vue/vue2-basic-component-property-$slots/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <Example>
+      <template #header>
+        <h2>header content</h2>
+      </template>
+      <template>
+        <p>main content</p>
+      </template>
+      <template #footer>
+        <p>footer content</p>
+      </template>
+      <template #aaa="{}">
+        <p>aaa</p>
+      </template>
+    </Example>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Example from "./Example.vue";
+
+export default Vue.extend({
+  components: {
+    Example,
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-component-property-$slots/src/Example.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      user: "Lee",
+    };
+  },
+  render(h) {
+    var header = this.$slots.header;
+    var body = this.$slots.default;
+    var footer = this.$slots.footer;
+
+    // not exist in $slots when has context.
+    console.log(this.$slots.aaa); // undefined
+
+    return h("div", [h("div", header), h("div", body), h("div", footer)]);
+  },
+});
+</script>
+
+<style></style>
