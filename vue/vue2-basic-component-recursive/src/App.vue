e01fa18399cb0ede8cc01c7ec11af8363fefc389
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-03 13:41:09 +0900

    feat(vue2): basic component recursive example

---
+++ vue/vue2-basic-component-recursive/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <Recursive
+      name="root"
+      :items="{ aaa: { a1: null, a2: null }, bbb: { b1: null } }"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Recursive from "./Recursive.vue";
+
+export default Vue.extend({
+  components: {
+    Recursive,
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-component-recursive/src/Recursive.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>name: {{ name }}</p>
+    <ul v-if="items">
+      <li v-for="(childItems, childName) in items" :key="childName">
+        <Recursive :items="childItems" :name="childName" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue/types/options";
+
+export default Vue.extend({
+  // alternative method for async loading component
+  // beforeCreate() {
+  //   this.$options.components.Recursive = require("./Recursive");
+  // },
+  components: {
+    Recursive: (() => import("./Recursive.vue")) as Component,
+  },
+  props: {
+    items: {
+      default: () => ({}),
+      type: Object,
+    },
+    name: {
+      default: "",
+      type: String,
+    },
+  },
+});
+</script>
+
+<style></style>
