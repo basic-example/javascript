469500beb8e9b3c5d5ebc3f892f18b170cdc28b8
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-03 10:05:21 +0900

    feat(vue2): basic directive v-for example

---
+++ vue/vue2-basic-directive-v-for/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <ol>
+      <li v-for="todo in todos" :key="todo.text">
+        {{ todo.text }}
+      </li>
+    </ol>
+    <ol>
+      <template v-for="todo in todos">
+        <li :key="todo.text">
+          {{ todo.text }}
+        </li>
+      </template>
+    </ol>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    todos: [
+      { text: "Learn JavaScript" },
+      { text: "Learn Vue" },
+      { text: "Build something awesome" },
+    ],
+  }),
+});
+</script>
+
+<style></style>
