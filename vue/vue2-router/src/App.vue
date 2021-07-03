5fbc96d123bc29b9047e6088e509f2e262a86b02
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-04 00:07:35 +0900

    feat(vue2): router example

---
+++ vue/vue2-router/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import VueRouter from "vue-router";
+import { RouteConfig } from "vue-router/types";
+
+const routes: RouteConfig[] = [];
+const router = new VueRouter({
+  routes,
+});
+
+export default Vue.extend({
+  router,
+});
+</script>
+
+<style>
+a {
+  display: block;
+}
+</style>
