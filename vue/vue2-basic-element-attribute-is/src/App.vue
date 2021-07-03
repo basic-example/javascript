58a5de41b6b5d52dd47eb2d04a2cd60ca3e19ed6
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-03 09:51:26 +0900

    feat(vue2): basic element attribute is example

---
+++ vue/vue2-basic-element-attribute-is/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <!--
+      code element is replaced to Child's host element and props is merged
+    -->
+    <code is="Child" abcd="abcd"></code>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Child from "./Child.vue";
+
+export default Vue.extend({
+  components: {
+    Child,
+  },
+});
+</script>
+
+<style></style>
---
+++ vue/vue2-basic-element-attribute-is/src/Child.vue
@@ -0,0 +1,13 @@
+<template>
+  <div a="aaa" b="bbb" c="ccc">
+    <h2>Child Component</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style></style>
