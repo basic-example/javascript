29f9c0600c32917e23a6db88ab7c3ff6160fe4f5
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-04 08:18:28 +0900

    feat(vue2): router RouteConfig children example

---
+++ vue/vue2-router-RouteConfig-children/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>App Component</h1>
+    <router-link to="/">/</router-link>
+    <router-link to="/parent">/parent</router-link>
+    <router-link to="/parent/child">/parent/child</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import VueRouter from "vue-router";
+import { RouteConfig } from "vue-router/types";
+
+const routes: RouteConfig[] = [
+  {
+    children: [
+      {
+        component: {
+          template: `
+            <div>
+              <h3>Child Component</h3>
+            </div>
+          `,
+        },
+        path: "child",
+      },
+    ],
+    component: {
+      template: `
+        <div>
+          <h2>Parent Component</h2>
+          <router-view></router-view>
+        </div>
+      `,
+    },
+    path: "/parent",
+  },
+];
+const router = new VueRouter({
+  routes,
+});
+
+export default Vue.extend({
+  router,
+});
+</script>
+
+<style>
+a {
+  display: block;
+}
+</style>
