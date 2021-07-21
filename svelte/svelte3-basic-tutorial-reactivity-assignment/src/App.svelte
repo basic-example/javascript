068bc144e9e39ad512fe9d2ee68becbd295ce5a0
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-22 02:36:44 +0900

    feat(svelte3): basic tutorial reactivity assignment example

---
+++ svelte/svelte3-basic-tutorial-reactivity-assignment/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let count = 0;
+  let incrementCount = () => {
+    count += 1;
+  };
+</script>
+
+<div>
+  <h1>App</h1>
+  <button on:click={incrementCount}>
+    Clicked {count}
+    {count === 1 ? "time" : "times"}
+  </button>
+</div>
+
+<style>
+</style>
