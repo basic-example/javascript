f51fbe3e652901c3c86ce90820adb5c708c7e2d4
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-22 02:55:15 +0900

    feat(svelte3): basic tutorial reactivity declaration example

---
+++ svelte/svelte3-basic-tutorial-reactivity-declaration/src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let count = 0;
+  $: doubled = count * 2;
+  let handleClick = () => {
+    count += 1;
+  };
+</script>
+
+<div>
+  <h1>App</h1>
+  <button on:click={handleClick}>
+    Clicked {count}
+    {count === 1 ? "time" : "times"}
+  </button>
+
+  <p>{count} doubled is {doubled}</p>
+</div>
+
+<style>
+</style>
