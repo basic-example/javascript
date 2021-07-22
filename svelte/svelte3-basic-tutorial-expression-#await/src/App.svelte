3bf44118ff27392de125990bf94475eca4e5994d
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-22 21:59:55 +0900

    feat(svelte3): basic tutorial expression #await example

---
+++ svelte/svelte3-basic-tutorial-expression-#await/src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  let getNewPromise = () => {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() > 0.3) {
+          reject("failed!!!");
+        } else {
+          resolve("success!!!");
+        }
+      }, 1000);
+    });
+    return promise;
+  };
+
+  let promise = getNewPromise();
+
+  function clickEvent() {
+    promise = getNewPromise();
+  }
+</script>
+
+<div>
+  <h1>App</h1>
+  {#await promise}
+    <p>...waiting</p>
+  {:then success}
+    <p>result: {success}</p>
+  {:catch error}
+    <p style="color: red">error: {error}</p>
+  {/await}
+  <button on:click={clickEvent}> set new promise </button>
+</div>
+
+<style>
+</style>
