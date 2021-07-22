817aac8d6ba4d0e6178a9df922c9db03a71b2934
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-22 22:48:03 +0900

    feat(svelte3): basic tutorial event handler example

---
+++ svelte/svelte3-basic-tutorial-event-handler/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let m = { x: 0, y: 0 };
+  let handleMousemove = (event) => {
+    m.x = event.clientX;
+    m.y = event.clientY;
+  };
+</script>
+
+<div>
+  <h1>App</h1>
+  <div on:mousemove={handleMousemove}>
+    The mouse position is {m.x} x {m.y}
+  </div>
+</div>
+
+<style>
+</style>
