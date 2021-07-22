68f4db8db8ed302f40eb5a93f8b9e0c1886ec484
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-22 22:56:17 +0900

    feat(svelte3): basic tutorial event inline handler example

---
+++ svelte/svelte3-basic-tutorial-event-inline-handler/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let m = { x: 0, y: 0 };
+</script>
+
+<div>
+  <h1>App</h1>
+  <div on:mousemove={(e) => (m = { x: e.clientX, y: e.clientY })}>
+    The mouse position is {m.x} x {m.y}
+  </div>
+</div>
+
+<style>
+</style>
