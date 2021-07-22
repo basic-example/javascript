6a7fd57b86e05e4be0352741fa756317047a5486
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-23 04:56:46 +0900

    feat(svelte3): basic tutorial event dispatch example

---
+++ svelte/svelte3-basic-tutorial-event-dispatch/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Child from "./Child.svelte";
+
+  let handleMessage = (event) => {
+    alert(event.detail.text);
+    console.log(event);
+  };
+</script>
+
+<div>
+  <h1>App</h1>
+  <Child on:message={handleMessage} />
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-event-dispatch/src/Child.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  let sayHello = () => {
+    dispatch("message", {
+      text: "Hello!",
+    });
+  };
+</script>
+
+<div>
+  <h2>Child</h2>
+  <button on:click={sayHello}>click button</button>
+</div>
+
+<style>
+</style>
