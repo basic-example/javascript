c327c5d9626d68ac995b74703f252e91479c3c4a
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-22 13:57:32 +0900

    feat(svelte3): basic tutorial props spread operator example

---
+++ svelte/svelte3-basic-tutorial-props-spread-operator/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Child from "./Child.svelte";
+
+  const info = {
+    name: "svelte",
+    version: 3,
+    website: "https://svelte.dev",
+  };
+</script>
+
+<div>
+  <h1>App</h1>
+  <Child {...info} />
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-props-spread-operator/src/Child.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let name;
+  export let version;
+  export let website;
+</script>
+
+<div>
+  <h2>Child</h2>
+  <div>
+    <p>name: {name}</p>
+    <p>version: {version}</p>
+    <p>website: {website}</p>
+  </div>
+</div>
+
+<style>
+</style>
