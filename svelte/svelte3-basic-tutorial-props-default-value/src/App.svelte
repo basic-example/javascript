a4ec7bc684015f0e527cf17947051e734ecdc0a1
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-22 13:45:47 +0900

    feat(svelte3): basic tutorial props default value example

---
+++ svelte/svelte3-basic-tutorial-props-default-value/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Parent from "./Parent.svelte";
+</script>
+
+<div>
+  <h1>App</h1>
+  <Parent message="hello world" />
+  <Parent />
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-props-default-value/src/Parent.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let message = "something...";
+</script>
+
+<div>
+  <h2>Parent</h2>
+  <p>{message}</p>
+</div>
+
+<style>
+</style>
