eb2ec6432d4a31f9db2c57300daa034b196cced9
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-22 14:24:45 +0900

    feat(svelte3): basic tutorial expression :else example

---
+++ svelte/svelte3-basic-tutorial-expression-：else/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let user = { loggedIn: false };
+  let toggle = () => {
+    user.loggedIn = !user.loggedIn;
+  };
+</script>
+
+<div>
+  <h1>App</h1>
+  {#if user.loggedIn}
+    <button on:click={toggle}> Log out </button>
+  {:else}
+    <button on:click={toggle}> Log in </button>
+  {/if}
+</div>
+
+<style>
+</style>
