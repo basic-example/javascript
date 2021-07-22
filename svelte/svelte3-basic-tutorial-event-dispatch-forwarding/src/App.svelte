86e67ced30af956748e12b5c9551287e7bfb52da
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-23 05:14:12 +0900

    feat(svelte3): basic tutorial event dispatch forwarding example

---
+++ svelte/svelte3-basic-tutorial-event-dispatch-forwarding/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Parent from "./Parent.svelte";
+
+  let handleMessage = (event) => {
+    alert(event.detail.text);
+    console.log(event);
+  };
+</script>
+
+<div>
+  <h1>App</h1>
+  <Parent on:message={handleMessage} />
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-event-dispatch-forwarding/src/Child.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  let sayHello = () => {
+    dispatch("message", {
+      text: "Hello!",
+    });
+  };
+</script>
+
+<div>
+  <h3>Child</h3>
+  <button on:click={sayHello}>click button</button>
+</div>
+
+<style>
+</style>
