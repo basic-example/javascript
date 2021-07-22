8e796a5b3745a4b5f4ebd891fc9dd07ee846d90f
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-22 21:23:25 +0900

    feat(svelte3): basic tutorial expression #each example

---
+++ svelte/svelte3-basic-tutorial-expression-#each/src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Child from "./Child.svelte";
+
+  let things = [
+    { id: "abcd", name: "apple" },
+    { id: "bcde", name: "banana" },
+    { id: "cdef", name: "carrot" },
+    { id: "defg", name: "doughnut" },
+    { id: "efgh", name: "egg" },
+  ];
+
+  let clickEvent = () => {
+    things = things.slice(1);
+  };
+</script>
+
+<div>
+  <h1>App</h1>
+  <h2>not keyed list</h2>
+  <ul>
+    {#each things as { name }, index}
+      <Child {name} />
+    {/each}
+  </ul>
+  <hr />
+  <h2>keyed list</h2>
+  <ul>
+    {#each things as thing (thing.id)}
+      <Child name={thing.name} />
+    {/each}
+  </ul>
+  <button on:click={clickEvent}>Remove first thing</button>
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-expression-#each/src/Child.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  const emojis = {
+    apple: "🍎",
+    banana: "🍌",
+    carrot: "🥕",
+    doughnut: "🍩",
+    egg: "🥚",
+  };
+
+  export let name;
+
+  const emoji = emojis[name];
+</script>
+
+<div>
+  <!-- both case same result -->
+  <!-- <p>
+    <span>{emojis[name]}</span>
+  </p> -->
+  <!-- both case same result -->
+  <!-- <p>
+    <span>{name}</span>
+  </p> -->
+  <!-- both case not same result -->
+  <p>
+    <span>{emoji}</span>
+  </p>
+</div>
+
+<style>
+</style>
