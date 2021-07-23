204c9e01a12fe3e4076c8b880eabfaf768ea3c74
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-23 16:02:12 +0900

    feat(svelte3): basic tutorial binding group example

---
+++ svelte/svelte3-basic-tutorial-binding-group/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  let sports = ["🚣🏻‍♀️", "🏊🏻‍♀️", "🏌🏻‍♂️", "🏄🏻‍♂️"];
+  let fruits = ["🍎", "🍇", "🍌", "🍍"];
+  let selectedSport = null;
+  let checkedFruits = [];
+</script>
+
+<div>
+  <h1>App</h1>
+  {#each sports as sport}
+    <label>
+      <input type="radio" bind:group={selectedSport} value={sport} />
+      {sport}
+    </label>
+  {/each}
+  <hr />
+  {#each fruits as fruit}
+    <label>
+      <input type="checkbox" bind:group={checkedFruits} value={fruit} />
+      {fruit}
+    </label>
+  {/each}
+  <p>selected: {selectedSport}</p>
+  <p>checkedList: {checkedFruits}</p>
+</div>
+
+<style>
+</style>
