db518ea2ba863fbd4ded6f221bbd47128ad81301
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-24 00:59:41 +0900

    feat(svelte3): basic tutorial lifecycle tick example

---
+++ svelte/svelte3-basic-tutorial-lifecycle-tick/src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { tick } from "svelte";
+  let num = 0;
+  let el: HTMLDivElement;
+</script>
+
+<div>
+  <h1>App</h1>
+  <button
+    on:click={async () => {
+      num++;
+      el = document.querySelector("#num");
+      console.log("before await tick()", el.innerText);
+      await tick();
+      console.log("after await tick()", el.innerText);
+    }}
+  >
+    increase
+  </button>
+  <div id="num">{num}</div>
+</div>
+
+<style>
+</style>
