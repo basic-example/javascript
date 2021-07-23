c7f8ef9991bd32948ce1174264bf87e80e3bb1f2
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-24 08:39:59 +0900

    feat(svelte3): basic tutorial store derived example

---
+++ svelte/svelte3-basic-tutorial-store-derived/src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { derived, writable } from "svelte/store";
+
+  const count = writable(0);
+
+  let increment = () => {
+    count.update((n) => n + 1);
+  };
+  count.subscribe((n) => {
+    alert(`count:${n}`);
+  });
+  const doubleCount = derived(count, (n) => n * 2);
+  doubleCount.subscribe((n) => {
+    alert(`doubleCount:${n}`);
+  });
+</script>
+
+<div>
+  <h1>App</h1>
+  <button on:click={increment}>+</button>
+</div>
+
+<style>
+</style>
