2e7775ea97b1aa8e709a7c34b2fbc892e25bcc19
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-23 23:02:18 +0900

    feat(svelte3): basic tutorial lifecycle onDestroy example

---
+++ svelte/svelte3-basic-tutorial-lifecycle-onDestroy/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Child from "./Child.svelte";
+  let isStarted = false;
+  let second = 0;
+</script>
+
+<div>
+  <h1>App</h1>
+  <button on:click={() => (isStarted = !isStarted)}>toggle</button>
+  <p>second: {second}</p>
+  {#if isStarted}
+    <Child
+      callback={() => {
+        second += 1;
+      }}
+    />
+  {/if}
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-lifecycle-onDestroy/src/Child.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onDestroy } from "svelte";
+
+  export let callback: () => void;
+
+  const interval = setInterval(callback, 1000);
+
+  onDestroy(() => {
+    clearInterval(interval);
+  });
+</script>
+
+<div>
+  <h2>Child</h2>
+</div>
