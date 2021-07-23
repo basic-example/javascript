92a95c9934456f0ce3436564a5cb6560e81fd172
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-23 23:04:09 +0900

    feat(svelte3): basic tutorial lifecycle onMount example

---
+++ svelte/svelte3-basic-tutorial-lifecycle-onMount/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  let message = "";
+  onMount(() => {
+    message = "Hello World";
+  });
+</script>
+
+<div>
+  <h1>App</h1>
+  <p>{message}</p>
+</div>
+
+<style>
+</style>
