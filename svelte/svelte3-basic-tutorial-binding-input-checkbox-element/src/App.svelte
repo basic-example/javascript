6518d0769c3c8d9015a5150c446054c950332390
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-23 21:49:55 +0900

    feat(svelte3): basic tutorial binding input checkbox element example

---
+++ svelte/svelte3-basic-tutorial-binding-input-checkbox-element/src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let agreement = false;
+</script>
+
+<div>
+  <h1>App</h1>
+  <label>
+    <input type="checkbox" bind:checked={agreement} />
+    agreement
+  </label>
+
+  {#if agreement}
+    <p>I am agreed</p>
+  {:else}
+    <p>I am disclaimed</p>
+  {/if}
+</div>
+
+<style>
+</style>
