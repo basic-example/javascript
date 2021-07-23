9a3cf4d7af59923f09aefd7e1442279cd779d466
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-23 21:54:18 +0900

    feat(svelte3): basic tutorial binding select element example

---
+++ svelte/svelte3-basic-tutorial-binding-select-element/src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  let selected = null;
+  let selectedList = [];
+  let options = ["apple", "banana", "orange"];
+</script>
+
+<div>
+  <h1>App</h1>
+  <select bind:value={selected}>
+    <option />
+    {#each options as option}
+      <option>{option}</option>
+    {/each}
+  </select>
+  <p>selected: {selected}</p>
+  <hr />
+  <select multiple bind:value={selectedList}>
+    <option />
+    {#each options as option}
+      <option>{option}</option>
+    {/each}
+  </select>
+  <p>selectedList: {selectedList}</p>
+</div>
+
+<style>
+</style>
