0f00e05abb9a713e8561b2d3122aafad910d4667
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-24 03:32:03 +0900

    feat(svelte3): basic tutorial store writable example

---
+++ svelte/svelte3-basic-tutorial-store-writable/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { writable } from "svelte/store";
+
+  const count = writable(0);
+
+  let decrement = () => {
+    count.update((n) => n - 1);
+  };
+  let increment = () => {
+    count.update((n) => n + 1);
+  };
+  let reset = () => {
+    count.set(0);
+  };
+  count.subscribe((n) => {
+    alert(n);
+  });
+</script>
+
+<div>
+  <h1>App</h1>
+  <button on:click={increment}>+</button>
+  <button on:click={decrement}>-</button>
+  <button on:click={reset}>reset</button>
+</div>
+
+<style>
+</style>
