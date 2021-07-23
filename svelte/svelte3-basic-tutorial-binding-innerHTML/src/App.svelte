51ac8183a4bb375afe1d359d2165aa7f12fa86e3
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-23 19:51:07 +0900

    feat(svelte3): basic tutorial binding innerHTML example

---
+++ svelte/svelte3-basic-tutorial-binding-innerHTML/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let html = "Hello <span style='color:red'>World</span>";
+</script>
+
+<div>
+  <h1>App</h1>
+  <div contenteditable="false" bind:innerHTML={html} />
+</div>
+
+<style>
+</style>
