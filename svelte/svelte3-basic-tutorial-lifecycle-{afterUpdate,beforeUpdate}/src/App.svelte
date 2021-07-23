4317be7920a99b379082cb99d9bfeef4ce5efc97
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-24 00:16:01 +0900

    feat(svelte3): basic tutorial lifecycle afterUpdate, beforeUpdate example

---
+++ svelte/svelte3-basic-tutorial-lifecycle-{afterUpdate,beforeUpdate}/src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { afterUpdate, beforeUpdate } from "svelte";
+  import Child from "./Child.svelte";
+
+  let AppNum = 0;
+  let propNum = 0;
+
+  afterUpdate(() => {
+    console.log("[App] afterUpdate");
+  });
+
+  beforeUpdate(() => {
+    console.log("[App] beforeUpdate");
+  });
+</script>
+
+<div>
+  <h1>App</h1>
+  <button on:click={() => AppNum++}>increase AppNum</button>
+  <button on:click={() => propNum++}>increase propNum</button>
+  <p>AppNum: {AppNum}</p>
+  <p>propNum: {propNum}</p>
+  <Child {propNum} />
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-lifecycle-{afterUpdate,beforeUpdate}/src/Child.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { afterUpdate, beforeUpdate } from "svelte";
+
+  let childNum = 0;
+  export let propNum;
+
+  afterUpdate(() => {
+    console.log("[Child] afterUpdate");
+  });
+
+  beforeUpdate(() => {
+    console.log("[Child] beforeUpdate");
+  });
+</script>
+
+<div>
+  <h2>Child</h2>
+  <button on:click={() => childNum++}>increase childNum</button>
+  <button on:click={() => propNum++}>increase propNum</button>
+  <p>childNum: {childNum}</p>
+  <p>propNum : {propNum}</p>
+</div>
+
+<style>
+</style>
