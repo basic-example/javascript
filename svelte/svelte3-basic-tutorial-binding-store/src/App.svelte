e02a881a92fedb06bd5fab5d79fb923a538e6d4b
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-24 09:25:09 +0900

    feat(svelte3): basic tutorial binding store example

---
+++ svelte/svelte3-basic-tutorial-binding-store/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { derived, writable } from "svelte/store";
+
+  const name = writable("World");
+  const message = derived(name, (name) => `Hello ${name}`);
+</script>
+
+<div>
+  <h1>App</h1>
+  <p>name: {$name}</p>
+  <p>message: {$message}</p>
+</div>
+
+<style>
+</style>
