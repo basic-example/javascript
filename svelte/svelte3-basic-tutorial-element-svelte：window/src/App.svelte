4041c5f0a1794804d3192d586c7c702a3af7fdd2
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-25 04:55:42 +0900

    feat(svelte3): basic tutorial element svelte:window example

---
+++ svelte/svelte3-basic-tutorial-element-svelte：window/src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  let y = 0;
+</script>
+
+<svelte:window
+  on:click={() => {
+    alert("clicked");
+  }}
+  bind:scrollY={y}
+/>
+<!--
+The list of properties you can bind to is as follows:
+- innerWidth
+- innerHeight
+- outerWidth
+- outerHeight
+- scrollX
+- scrollY
+- online — an alias for window.navigator.onLine
+-->
+<div>
+  <h1>App</h1>
+  <p>please click browser window~!</p>
+</div>
+
+<style>
+</style>
