ecaa9ad8abffb8ceb78bcbbbc8e47d1a7eddc045
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-25 00:29:35 +0900

    feat(svelte3): basic tutorial getContext, setContext example

---
+++ svelte/svelte3-basic-tutorial-{getContext,setContext}/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { setContext } from "svelte";
+  import Child from "./Child.svelte";
+
+  setContext("something", {
+    message: "hello world",
+  });
+</script>
+
+<div>
+  <h1>App</h1>
+  <Child />
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-{getContext,setContext}/src/Child.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+
+  const { message } = getContext("something");
+</script>
+
+<div>
+  <h2>Child</h2>
+  <p>{message}</p>
+</div>
+
+<style>
+</style>
