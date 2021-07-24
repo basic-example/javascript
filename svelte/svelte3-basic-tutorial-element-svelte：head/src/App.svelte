a53444bdac13d45356d5012e0a97669f638411ec
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-25 05:32:09 +0900

    feat(svelte3): basic tutorial element svelte:head example

---
+++ svelte/svelte3-basic-tutorial-element-svelte：head/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" href="/build/bundle.css" />
+</svelte:head>
+
+<div>
+  <h1>App</h1>
+</div>
+
+<style>
+  h1 {
+    color: blue;
+  }
+</style>
