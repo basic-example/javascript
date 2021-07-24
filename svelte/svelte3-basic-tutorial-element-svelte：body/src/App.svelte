306c551237d9709144ed58c0d2f9bd3f79f2d0e0
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-25 05:22:42 +0900

    feat(svelte3): basic tutorial element svelte:body example

---
+++ svelte/svelte3-basic-tutorial-element-svelte：body/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let y = 0;
+</script>
+
+<svelte:body
+  on:click={() => {
+    alert("clicked");
+  }} />
+<div>
+  <h1>App</h1>
+  <p>please click document.body</p>
+</div>
+
+<style>
+</style>
