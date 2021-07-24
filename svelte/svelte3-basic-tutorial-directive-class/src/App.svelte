88aca592c827f96da4e895bb3d00bb960c4695ae
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-24 09:41:29 +0900

    feat(svelte3): basic tutorial directive class example

---
+++ svelte/svelte3-basic-tutorial-directive-class/src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  let color = "blue";
+  let align = "center";
+  let blue = true;
+</script>
+
+<div>
+  <h1>App</h1>
+  <p class={color === "blue" ? "blue" : "red"}>blue</p>
+  <p class={align === "center" ? "text-center" : "text-left"}>center</p>
+  <p class:blue={color === "blue"}>blue</p>
+  <p class:text-center={align === "center"}>center</p>
+  <p class:blue>blue</p>
+</div>
+
+<style>
+  .text-center {
+    text-align: center;
+  }
+  .text-left {
+    text-align: left;
+  }
+  .red {
+    color: red;
+  }
+  .blue {
+    color: blue;
+  }
+</style>
