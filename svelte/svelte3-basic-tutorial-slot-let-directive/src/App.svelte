225c89caaa5be41dd8d400427e2565986efcfd41
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-25 00:06:45 +0900

    feat(svelte3): basic tutorial slot let directive example

---
+++ svelte/svelte3-basic-tutorial-slot-let-directive/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Child from "./Child.svelte";
+</script>
+
+<div>
+  <h1>App</h1>
+  <Child let:hovered={activated}>
+    <div class:active={activated}>
+      <p>hello world</p>
+    </div>
+  </Child>
+</div>
+
+<style>
+  .active {
+    background-color: #ff3e00;
+    color: white;
+  }
+</style>
---
+++ svelte/svelte3-basic-tutorial-slot-let-directive/src/Child.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let isHover;
+
+  let enter = () => {
+    isHover = true;
+  };
+
+  let leave = () => {
+    isHover = false;
+  };
+</script>
+
+<div>
+  <h2>Child</h2>
+  <div on:mouseenter={enter} on:mouseleave={leave}>
+    <slot hovered={isHover} />
+  </div>
+</div>
+
+<style>
+</style>
