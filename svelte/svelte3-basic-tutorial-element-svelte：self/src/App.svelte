9e6a15cebb392f640e33f6f270dc598e500ed182
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-25 04:10:57 +0900

    feat(svelte3): basic tutorial element svelte:self example

---
+++ svelte/svelte3-basic-tutorial-element-svelte：self/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { File } from "./Folder.svelte";
+  import Folder from "./Folder.svelte";
+
+  let files: Array<File> = [
+    {
+      name: "a",
+      files: [
+        {
+          name: "aa",
+          files: [
+            {
+              name: "aaa",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "b",
+    },
+  ];
+</script>
+
+<div>
+  <h1>App</h1>
+  <Folder {files} />
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-element-svelte：self/src/Folder.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+  export type File = { name: string; files?: Array<File> };
+</script>
+
+<script lang="ts">
+  export let files: Array<File>;
+</script>
+
+<div style="margin-left:15px">
+  <h2>Folder</h2>
+  {#each files as something}
+    {#if something.files}
+      <p>folder: {something.name}</p>
+      <svelte:self files={something.files} />
+    {:else}
+      <p>file: {something.name}</p>
+    {/if}
+  {/each}
+</div>
+
+<style>
+</style>
