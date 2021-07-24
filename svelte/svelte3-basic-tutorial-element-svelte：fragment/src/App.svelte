24bb198cda98a7c0e997d78cb191345180498980
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-25 05:50:56 +0900

    feat(svelte3): basic tutorial element svelte:fragment example

---
+++ svelte/svelte3-basic-tutorial-element-svelte：fragment/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Child from "./Child.svelte";
+</script>
+
+<div>
+  <h1>App</h1>
+  <Child>
+    <svelte:fragment slot="main">
+      <p>hello world</p>
+      <p>hello world</p>
+    </svelte:fragment>
+  </Child>
+</div>
+
+<style>
+</style>
