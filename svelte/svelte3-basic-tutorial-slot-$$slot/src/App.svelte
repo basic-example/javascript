df65fa9de563af29be1cf89ad2e16bc62e6263a3
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-24 17:20:20 +0900

    feat(svelte3): basic tutorial slot 18576slot example

---
+++ svelte/svelte3-basic-tutorial-slot-$$slot/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Child from "./Child.svelte";
+</script>
+
+<div>
+  <h1>App</h1>
+  <Child>
+    <div slot="main">main content</div>
+  </Child>
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-slot-$$slot/src/Child.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<div>
+  <h2>Child</h2>
+  <slot name="something" />
+  <slot name="main" />
+  <p>$$slots.something: {$$slots.something}</p>
+  <p>$$slots.main: {$$slots.main}</p>
+</div>
+
+<style>
+</style>
