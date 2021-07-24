28affb0d6e74ec30cda7a2ab0158b484e2a6a7cd
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-24 14:40:41 +0900

    feat(svelte3): basic tutorial slot fallback example

---
+++ svelte/svelte3-basic-tutorial-slot-fallback/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Child from "./Child.svelte";
+</script>
+
+<div>
+  <h1>App</h1>
+  <Child />
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-slot-fallback/src/Child.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div>
+  <h2>Child</h2>
+  <slot>
+    <p>[fallback] Hello World</p>
+  </slot>
+</div>
+
+<style>
+</style>
