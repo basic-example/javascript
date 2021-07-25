79958c7440155687cfb9ba14ae73cac6bbff6fad
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-25 16:03:11 +0900

    feat(svelte3): basic tutorial script tag context="module" example

---
+++ svelte/svelte3-basic-tutorial-script-tag-context-module/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Child from "./Child.svelte";
+  let show = true;
+</script>
+
+<div>
+  <h1>App</h1>
+  <button
+    on:click={() => {
+      show = !show;
+    }}
+  >
+    toggle
+  </button>
+  {#if show}
+    <Child />
+  {/if}
+</div>
+
+<style>
+</style>
---
+++ svelte/svelte3-basic-tutorial-script-tag-context-module/src/Child.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+  let count = 0;
+</script>
+
+<script lang="ts">
+  count++;
+</script>
+
+<div>
+  <h2>Child</h2>
+  <p>count: {count}</p>
+</div>
+
+<style>
+</style>
