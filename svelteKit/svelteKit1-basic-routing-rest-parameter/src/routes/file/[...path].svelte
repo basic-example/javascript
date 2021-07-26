0c1b51b6c897ebaba8076c2050bfb80a8374f0f7
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-26 20:51:59 +0900

    feat(svelteKit1): basic routing rest parameter example

---
+++ svelteKit/svelteKit1-basic-routing-rest-parameter/src/routes/file/[...path].svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit";
+
+  export const load: Load = ({ page, fetch, session, context }) => {
+    return {
+      props: {
+        params: page.params,
+      },
+    };
+  };
+</script>
+
+<script lang="ts">
+  export let params;
+</script>
+
+<h1>File [...Path]</h1>
+<p>params: {JSON.stringify(params)}</p>
+
+<style>
+</style>
---
+++ svelteKit/svelteKit1-basic-routing-rest-parameter/src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<h1>App</h1>
+<nav>
+  <p><a href="/aaa/bbb/ccc/file">/aaa/bbb/ccc/file</a></p>
+  <p><a href="/file/ddd/fff/ggg/">/file/ddd/fff/ggg</a></p>
+</nav>
+
+<style>
+</style>
