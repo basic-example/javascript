ccf1666dc4d7f071b0090185f11b6a3c4be76a22
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-27 02:42:01 +0900

    feat(svelteKit1): basic routing error example

---
+++ svelteKit/svelteKit1-basic-routing-error/src/routes/__error.svelte
@@ -0,0 +1,18 @@
+<script context="module" lang="ts">
+  import type { ErrorLoad } from "@sveltejs/kit";
+
+  export const load: ErrorLoad = ({ error, status }) => {
+    return {
+      props: {
+        title: `${status}: ${error.message}`,
+      },
+    };
+  };
+</script>
+
+<script>
+  export let title;
+</script>
+
+<h1>Root Error Page</h1>
+<p>{title}</p>
---
+++ svelteKit/svelteKit1-basic-routing-error/src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<h1>App</h1>
+<nav>
+  <p><a href="/not-found">/not-found</a></p>
+</nav>
+
+<style>
+</style>
