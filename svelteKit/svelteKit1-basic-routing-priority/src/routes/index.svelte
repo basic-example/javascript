ab70b8bda4ac4c7535192dd481d0ae7c33ebb92a
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-27 01:40:50 +0900

    feat(svelteKit1): basic routing priority example

---
+++ svelteKit/svelteKit1-basic-routing-priority/src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<h1>App</h1>
+<p><a href="/aaa/abc">/aaa/abc</a>(alphabet asc order has higher priority)</p>
+<p><a href="/bbb/abc">/bbb/abc</a>(.ts,.js is higer priority than .svelte)</p>
+
+<style>
+</style>
