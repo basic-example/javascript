8af916d9397140fd1e119ee6e17c9e04f17cd1c8
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-27 17:07:05 +0900

    feat(svelteKit1): basic load function example

---
+++ svelteKit/svelteKit1-basic-load-function/src/routes/index.svelte
@@ -0,0 +1,42 @@
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit";
+  export const load: Load = ({
+    // page: {
+    // 	host: string;
+    // 	path: string;
+    // 	params: PageParams;
+    // 	query: URLSearchParams;
+    // };
+    // fetch: (info: RequestInfo, init?: RequestInit) => Promise<Response>;
+    // session: Session;
+    // context: Context;
+    page,
+    fetch,
+    session,
+    context,
+  }) => {
+    console.log(page, fetch, session, context);
+
+    return {
+      // status?: number;
+      // error?: string | Error;
+      // redirect?: string;
+      // props?: Props;
+      // context?: Context;
+      // maxage?: number;
+      props: {
+        message: "Hello World",
+      },
+    };
+  };
+</script>
+
+<script lang="ts">
+  export let message;
+</script>
+
+<h1>App</h1>
+<p>{message}</p>
+
+<style>
+</style>
