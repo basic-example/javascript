901cc1a10b32557bce82125377204b3f0d9c482e
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-27 19:38:54 +0900

    feat(svelteKit1): basic load function page parameter example

---
+++ svelteKit/svelteKit1-basic-load-function-page-parameter/src/routes/[...qux].svelte
@@ -0,0 +1,29 @@
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit";
+  export const load: Load = ({ page: { host, path, params, query } }) => {
+    return {
+      props: {
+        host: host,
+        path: path,
+        params: params,
+        query: query,
+      },
+    };
+  };
+</script>
+
+<script lang="ts">
+  export let host;
+  export let path;
+  export let params;
+  export let query;
+</script>
+
+<h1>[...qux]</h1>
+<p>host: {host}</p>
+<p>path: {path}</p>
+<p>params: {JSON.stringify(params)}</p>
+<p>query: {query}</p>
+
+<style>
+</style>
---
+++ svelteKit/svelteKit1-basic-load-function-page-parameter/src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<h1>App</h1>
+<p>
+  <a href="/abc?a=aaa&b=bbb#fff">
+    <span>/test?a=aaa&b=bbb#fff</span>
+  </a>
+</p>
+<p>
+  <a href="/abc-dgf?a=aaa&b=bbb#fff">
+    <span>/abc-dgf?a=aaa&b=bbb#fff</span>
+  </a>
+</p>
+<p>
+  <a href="/xyz/abcd/1234?a=aaa&b=bbb#fff">
+    <span>/xyz/abcd/1234?a=aaa&b=bbb#fff</span>
+  </a>
+</p>
+
+<style>
+</style>
