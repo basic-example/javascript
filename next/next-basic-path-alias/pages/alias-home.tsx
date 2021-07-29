09740ed937cdda4435c4f691526248d3a7a109a1
jongwoo Yoo <dbwhddn10@gmail.com>
2021-07-29 22:51:17 +0900

    revert(next-basic-path-alias): delete example

--- next/next-basic-path-alias/pages/alias-home.tsx
+++
@@ -1,5 +0,0 @@
-import * as Home from "./home";
-
-export default Home.default;
-
-export const getServerSideProps = Home.getServerSideProps;
--- next/next-basic-path-alias/pages/alias-user/[id].tsx
+++
@@ -1,5 +0,0 @@
-import * as UserId from "../user/[id]";
-
-export default UserId.default;
-export const getStaticProps = UserId.getStaticProps;
-export const getStaticPaths = UserId.getStaticPaths;
