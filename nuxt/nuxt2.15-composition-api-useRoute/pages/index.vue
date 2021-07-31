4445fece6280bba17e6566448fb4bfc02f96f79c
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-01 06:12:43 +0900

    feat(nuxt2.15): composition api useRoute example

---
+++ nuxt/nuxt2.15-composition-api-useRoute/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Root Page</h1>
+    <nav>
+      <p><a href="/users/1">/users/1</a></p>
+      <p><a href="/users/2">/users/2</a></p>
+      <p><a href="/users/3">/users/3</a></p>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ nuxt/nuxt2.15-composition-api-useRoute/pages/users/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>User</h1>
+    <p>ID: {{ id }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useRoute, computed } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  setup() {
+    const route = useRoute();
+    const id = computed(() => route.value.params.id);
+
+    return {
+      id,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
