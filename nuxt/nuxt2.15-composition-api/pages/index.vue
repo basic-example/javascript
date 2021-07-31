800262c75bda063f95494ac29d89b5e03098ec82
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-01 05:40:42 +0900

    feat(nuxt2.15): composition api example

---
+++ nuxt/nuxt2.15-composition-api/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Root Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
