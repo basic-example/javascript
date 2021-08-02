aaf04c7ffa39dda3467491d92ee9a506efe09b79
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-02 14:59:14 +0900

    feat(nuxt2.15): composition api useMeta example

---
+++ nuxt/nuxt2.15-composition-api-useMeta/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Root Page</h1>
+    <button @click="setTitle">setTitle(error)</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useMeta } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  head: {},
+  setup() {
+    // it works
+    useMeta().title.value = "My Custom Title1";
+    // it works. equivalent to the above
+    useMeta({ title: "My Custom Title2" });
+
+    return {
+      setTitle: () => {
+        // it does not work. because it called outside of component
+        useMeta().title.value = "My Custom Title1";
+      },
+    };
+  },
+});
+</script>
+
+<style scoped></style>
