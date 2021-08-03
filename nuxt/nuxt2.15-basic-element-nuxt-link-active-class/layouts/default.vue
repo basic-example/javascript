9e462a0da5ec3c46d6169b255a27f5a43cbbedf2
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-04 08:46:17 +0900

    feat(nuxt2.15): basic element nuxt-link active class example

---
+++ nuxt/nuxt2.15-basic-element-nuxt-link-active-class/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">/</NuxtLink>
+      <NuxtLink to="/hello">/hello</NuxtLink>
+      <NuxtLink to="/hello/world">/hello/world</NuxtLink>
+    </nav>
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { ComponentOptions } from "vue";
+
+export default {} as ComponentOptions<Vue>;
+</script>
+
+<style scoped>
+a.nuxt-link-active {
+  font-weight: bold;
+  font-size: 2em;
+}
+a.nuxt-link-exact-active {
+  color: red;
+}
+</style>
---
+++ nuxt/nuxt2.15-basic-element-nuxt-link-active-class/pages/hello.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <NuxtChild />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { ComponentOptions } from "vue";
+
+export default {} as ComponentOptions<Vue>;
+</script>
+
+<style scoped></style>
