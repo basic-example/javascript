99dec66e7e3236c77a4518d03e856191ecaa8084
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-04 07:29:34 +0900

    feat(nuxt2.15): basic page dynmaic example

---
+++ nuxt/nuxt2.15-basic-page-dynamic/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Root Page</h1>
+    <nav>
+      <a href="/users/1">/users/1</a>
+      <a href="/users/2">/users/2</a>
+      <a href="/users/3">/users/3</a>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { ComponentOptions } from "vue";
+
+export default {} as ComponentOptions<Vue>;
+</script>
+
+<style scoped></style>
---
+++ nuxt/nuxt2.15-basic-page-dynamic/pages/users/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>User</h1>
+    <p>ID: {{ $route.params.id }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { ComponentOptions } from "vue";
+
+export default {} as ComponentOptions<Vue>;
+</script>
+
+<style scoped></style>
