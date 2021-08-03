290deb73372f2f9a2e0e1a853264211cf2c44d61
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-04 03:32:28 +0900

    feat(nuxt2.15): basic property asyncData example

---
+++ nuxt/nuxt2.15-basic-property-asyncData/components/message.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Message</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { ComponentOptions } from "vue";
+
+export default {
+  async asyncData() {
+    const message = await new Promise((resolve) => {
+      resolve("hello world");
+    });
+
+    // eslint-disable-next-line no-console
+    console.log("asyncData is run in message.vue");
+
+    return { message };
+  },
+} as ComponentOptions<Vue>;
+</script>
+
+<style scoped></style>
---
+++ nuxt/nuxt2.15-basic-property-asyncData/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Root Page</h1>
+    <p>asyncData only work in pages</p>
+    <p>{{ JSON.stringify(user) }}</p>
+    <Message />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { ComponentOptions } from "vue";
+import type { Context } from "@nuxt/types";
+import Message from "../components/message.vue";
+
+export default {
+  components: {
+    Message,
+  },
+  async asyncData(context: Context) {
+    const url = `https://jsonplaceholder.typicode.com/users/1`;
+    const response = await fetch(url);
+    const user = await response.json();
+
+    // eslint-disable-next-line no-console
+    console.log(Object.keys(context));
+
+    return { user };
+  },
+} as ComponentOptions<Vue>;
+</script>
+
+<style scoped></style>
