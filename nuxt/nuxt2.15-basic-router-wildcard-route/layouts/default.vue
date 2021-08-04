8037c32c2c8305a71150ef3d5dd679a2fbbd4fa2
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-05 03:32:41 +0900

    feat(nuxt2.15): basic router wildcard route example

---
+++ nuxt/nuxt2.15-basic-router-wildcard-route/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/xxx">/xxx</NuxtLink>
+      <NuxtLink to="/hello/xxx">/hello/xxx</NuxtLink>
+      <NuxtLink to="/hello/world">/hello/world</NuxtLink>
+    </nav>
+    <Nuxt />
+  </div>
+</template>
---
+++ nuxt/nuxt2.15-basic-router-wildcard-route/pages/hello/world.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { NuxtAppOptions } from "@nuxt/types";
+
+export default {} as NuxtAppOptions;
+</script>
+
+<style scoped></style>
