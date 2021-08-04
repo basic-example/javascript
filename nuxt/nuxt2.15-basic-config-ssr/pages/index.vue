5b8993f5738de1a869a79096accf08e30193e7b7
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-04 23:03:20 +0900

    feat(nuxt2.15): basic config ssr example

---
+++ nuxt/nuxt2.15-basic-config-ssr/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Root Page</h1>
+    <p>renderOn: {{ renderOn }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { NuxtAppOptions } from "@nuxt/types";
+
+export default {
+  data() {
+    return {
+      renderOn: null,
+    };
+  },
+  fetch() {
+    this.renderOn = process.server ? "server" : "client";
+  },
+} as NuxtAppOptions;
+</script>
+
+<style scoped></style>
