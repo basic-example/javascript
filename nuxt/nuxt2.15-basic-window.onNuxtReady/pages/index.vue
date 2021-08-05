9a9851812bc4ced7afbfa5a6184309d1879a24d9
jongwoo Yoo <dbwhddn10@gmail.com>
2021-08-05 10:27:36 +0900

    feat(nuxt2.15): basic window.onNuxtReady example

---
+++ nuxt/nuxt2.15-basic-window.onNuxtReady/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Root Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { NuxtAppOptions } from "@nuxt/types";
+
+export default {
+  mounted() {
+    window.onNuxtReady(() => {
+      alert("Nuxt is ready and mounted");
+    });
+  },
+} as NuxtAppOptions;
+</script>
+
+<style scoped></style>
