4125c3fbbd483cfdb5722612e1c1e2caa1e5b1d0
jongwoo Yoo <dbwhddn10@gmail.com>
2022-12-17 00:36:37 +0900

    chore(root): edit .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,44 +1,37 @@
 module.exports = (files) => {
+  const fs = require("fs");
   const sep = require("path").sep;
   const exec = require("child_process").exec;
-  const obj = files.reduce((acc, value) => {
-    const root = __dirname.replace(new RegExp("\\" + sep, "g"), "/");
-    const rel = value.replace(root + "/", "");
-    const segs = rel.split("/");
-    const type = segs.shift();
-    const proj = segs.shift();
-    const path = segs.join("/");
-
-    if (!path) {
-      return acc;
+  const root = __dirname.replace(new RegExp("\\" + sep, "g"), "/");
+  const dirs = files.reduce((acc, value) => {
+    const segs = value.replace(root + "/", "").split("/");
+    while (segs.pop()) {
+      const path = segs.join("/");
+      if (path && !acc.includes(path)) {
+        acc.push(path);
+      }
     }
-
-    if (Object.keys(acc).indexOf(type) === -1) {
-      acc[type] = [];
-    }
-
-    if (acc[type].indexOf(proj) === -1) {
-      acc[type].push(proj);
-    }
-
     return acc;
-  }, {});
+  }, []);
+  const project_dirs = dirs.filter((path) =>
+    fs.existsSync(root + "/" + path + "/package.json")
+  );
+  project_dirs.forEach((dir) => {
+    const cmd =
+      dir
+        .split("/")
+        .map((folder) => `cd ${folder} && `)
+        .join("") + "npm install && npm run";
 
-  for (const [type, projects] of Object.entries(obj)) {
-    projects.forEach((project) => {
-      const cmd = `cd ${type} && cd ${project} && npm install && npm run lint`;
-
-      console.log("----------------------------------------------------------");
-      console.log(`run: ` + cmd);
-      console.log("----------------------------------------------------------");
-
-      exec(cmd, (error) => {
-        if (error) {
-          throw new Error(error);
-        }
-      });
+    console.log("=============================");
+    console.log(cmd);
+    console.log("=============================");
+    exec(cmd, (error) => {
+      if (error) {
+        throw new Error(error);
+      }
     });
-  }
+  });
 
   return [];
 };
