06ffe602958e3a585b6fb618f999bea57634afc2
jongwoo Yoo <dbwhddn10@gmail.com>
2022-12-17 19:41:16 +0900

    feat(puppeteer): edit launch example

--- puppeteer/launch/app.test.ts
+++ puppeteer/launch/app.test.ts
@@ -2,18 +2,15 @@ import puppeteer from 'puppeteer';
 
 describe('browser launch `http://google.com`', () => {
   test('it contain `google` text', async () => {
-    const crawler = async () => {
-      const browser = await puppeteer.launch({
-        // headless: false,
-      });
-      const page = await browser.newPage();
-      await page.goto('http://google.com');
+    const browser = await puppeteer.launch({
+      // headless: false,
+    });
+    const page = await browser.newPage();
+    await page.goto('http://google.com');
 
-      expect(await page.content()).toContain('google');
+    expect(await page.content()).toContain('google');
 
-      await page.close();
-      await browser.close();
-    };
-    await crawler();
+    await page.close();
+    await browser.close();
   });
 });
