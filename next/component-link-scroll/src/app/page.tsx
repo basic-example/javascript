63d40c91c9160c88c85514962a9c349a4e590175
jongwoo Yoo <dbwhddn10@gmail.com>
2023-11-08 17:01:12 +0900

    feat(next): add component-link-scroll example

---
+++ next/component-link-scroll/src/app/page.tsx
@@ -0,0 +1,107 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>please scroll down~!</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <nav>
+        <ul>
+          <li>
+            <Link href="/dashboard">scroll default</Link>
+          </li>
+          <li>
+            <Link href="/dashboard" scroll={false}>
+              scroll false
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
