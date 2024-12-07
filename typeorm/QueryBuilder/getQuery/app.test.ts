fa4cda60e6fb6e5356b50e8239b7313648e34251
jongwoo Yoo <dbwhddn10@gmail.com>
2024-12-07 11:56:05 +0900

    feat(typeorm): add QueryBuilder/getQuery example

---
+++ typeorm/QueryBuilder/getQuery/app.test.ts
@@ -0,0 +1,43 @@
+import {
+  BaseEntity,
+  Column,
+  DataSource,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+}
+
+describe("typeorm", () => {
+  test("QueryBuilder/getQuery", async () => {
+    await new DataSource({
+      type: "sqlite",
+      database: ":memory:",
+      entities: [User],
+      logging: false,
+      synchronize: true,
+    }).initialize();
+
+    const queryBuilder = (await User.getRepository())
+      .createQueryBuilder("aaa")
+      .update()
+      .set({ firstName: "bbb", lastName: "ccc" });
+
+    expect(queryBuilder.getQuery()).toBe(
+      'UPDATE "user" SET "firstName" = :orm_param_0, "lastName" = :orm_param_1',
+    );
+    expect(queryBuilder.getQuery()).toBe(
+      'UPDATE "user" SET "firstName" = :orm_param_2, "lastName" = :orm_param_3',
+    );
+  });
+});
