8ea9e47da72e9e485c2a3b8b44cec0ddfdb922bd
jongwoo Yoo <dbwhddn10@gmail.com>
2024-12-09 17:01:18 +0900

    feat(typeorm): add QueryBuilder/getOne example

---
+++ typeorm/QueryBuilder/getOne/app.test.ts
@@ -0,0 +1,62 @@
+import {
+  AbstractLogger,
+  BaseEntity,
+  Column,
+  DataSource,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+}
+
+describe("typeorm", () => {
+  test("QueryBuilder/getOne", async () => {
+    let selectQuery;
+
+    class Logger extends AbstractLogger {
+      protected writeLog(level, logMessage, queryRunner) {}
+
+      logQuery(query, parameters, queryRunner) {
+        if (query.match(/^SELECT "/)) {
+          selectQuery = query;
+        }
+      }
+    }
+
+    await new DataSource({
+      type: "sqlite",
+      database: ":memory:",
+      entities: [User],
+      logging: true,
+      logger: new Logger(),
+      synchronize: true,
+    }).initialize();
+
+    const user1 = new User();
+    user1.firstName = "qqq";
+    user1.lastName = "www";
+    await user1.save();
+
+    const user2 = new User();
+    user2.firstName = "eee";
+    user2.lastName = "rrr";
+    await user2.save();
+
+    const result = await User.getRepository()
+      .createQueryBuilder("aaa")
+      .getOne();
+
+    expect(selectQuery).toMatch(/FROM "user" "aaa"$/);
+    expect(result).toStrictEqual(user1);
+  });
+});
