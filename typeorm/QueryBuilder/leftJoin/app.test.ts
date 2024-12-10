e4d51dd8fa0cdb1676d19f499c08753bce07dc91
jongwoo Yoo <dbwhddn10@gmail.com>
2024-12-10 11:22:57 +0900

    feat(typeorm): add QueryBuilder/leftJoin example

---
+++ typeorm/QueryBuilder/leftJoin/app.test.ts
@@ -0,0 +1,92 @@
+import {
+  AbstractLogger,
+  BaseEntity,
+  Column,
+  DataSource,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @OneToMany(() => Photo, (photo) => photo.user)
+  photos: Photo[];
+}
+
+@Entity()
+export class Photo extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  url: string;
+
+  @ManyToOne(() => User, (user) => user.photos)
+  @JoinColumn({
+    name: "user_id",
+    referencedColumnName: "id",
+  })
+  user: User;
+}
+
+describe("typeorm", () => {
+  test("QueryBuilder/leftJoin", async () => {
+    let selectQuery;
+
+    class Logger extends AbstractLogger {
+      protected writeLog(level, logMessage, queryRunner) {}
+
+      logQuery(query, parameters, queryRunner) {
+        query = query.replace("?", () => {
+          return "'" + parameters.shift() + "'";
+        });
+        if (query.match(/^SELECT/)) {
+          selectQuery = query;
+        }
+      }
+    }
+
+    await new DataSource({
+      type: "sqlite",
+      database: ":memory:",
+      entities: [User, Photo],
+      logger: new Logger(),
+      logging: false,
+      synchronize: true,
+    }).initialize();
+
+    const query1 = await User.getRepository()
+      .createQueryBuilder("u")
+      .select("1")
+      .leftJoin("u.photos", "p");
+
+    await expect(query1.getMany()).resolves.not.toThrow();
+    expect(selectQuery).toBe(
+      'SELECT 1 FROM "user" "u" LEFT JOIN "photo" "p" ON "p"."user_id"="u"."id"',
+    );
+
+    const query2 = await User.getRepository()
+      .createQueryBuilder("u")
+      .select("1")
+      .leftJoin("u.photos", "p", "u.firstName = :name", {
+        name: "qqq",
+      });
+
+    await expect(query2.getMany()).resolves.not.toThrow();
+    expect(selectQuery).toBe(
+      'SELECT 1 FROM "user" "u" LEFT JOIN "photo" "p" ON "p"."user_id"="u"."id" AND ("u"."firstName" = \'qqq\')',
+    );
+  });
+});
