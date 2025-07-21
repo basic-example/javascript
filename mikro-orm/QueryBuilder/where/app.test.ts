fc9e2c19bf72e16eba053f294cc747ad6dae29f4
jongwoo Yoo <dbwhddn10@gmail.com>
2025-07-22 04:07:23 +0900

    feat(mikro-orm): add QueryBuilder/where example

---
+++ mikro-orm/QueryBuilder/where/app.test.ts
@@ -0,0 +1,38 @@
+import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/core';
+import { EntityManager, SqliteDriver } from '@mikro-orm/sqlite';
+
+let orm: MikroORM;
+
+@Entity()
+export class User {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+beforeAll(async () => {
+  const config = {
+    entities: [User],
+    dbName: ':memory:',
+    driver: SqliteDriver,
+    allowGlobalContext: true,
+  } as Parameters<typeof MikroORM.init<SqliteDriver>>[0];
+  orm = await MikroORM.init(config);
+  await orm.getSchemaGenerator().createSchema();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('app', async () => {
+  const em = orm.em as EntityManager;
+  const qb = em.createQueryBuilder(User).where({ name: 'abcd' });
+
+  expect(qb.getQuery()).toEqual(
+    'select `u0`.* from `user` as `u0` where `u0`.`name` = ?',
+  );
+  expect(qb.getParams()).toEqual(['abcd']);
+});
