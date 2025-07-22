5f371939a3f8fe6f1d876584868a475e9a95e969
jongwoo Yoo <dbwhddn10@gmail.com>
2025-07-22 17:33:38 +0900

    feat(mikro-orm): add EntityRepository/map example

---
+++ mikro-orm/EntityRepository/map/app.test.ts
@@ -0,0 +1,38 @@
+import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/core';
+import { EntityManager, SqliteDriver } from '@mikro-orm/sqlite';
+
+let orm: MikroORM;
+
+@Entity()
+export class User {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+beforeAll(async () => {
+  const config = {
+    entities: [User],
+    dbName: ':memory:',
+    driver: SqliteDriver,
+    allowGlobalContext: true,
+  } as Parameters<typeof MikroORM.init<SqliteDriver>>[0];
+  orm = await MikroORM.init(config);
+  await orm.getSchemaGenerator().createSchema();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('app', async () => {
+  const em = orm.em as EntityManager;
+  const repo = em.getRepository(User);
+  const users = [{ id: 1, name: 'abcd' }].map((user) => repo.map(user));
+
+  expect(users[0]).toBeInstanceOf(User);
+  expect(users[0].id).toBe(1);
+  expect(users[0].name).toBe('abcd');
+});
