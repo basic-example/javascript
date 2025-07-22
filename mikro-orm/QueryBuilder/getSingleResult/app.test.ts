8cd8549b3b2f7b8409f36e75dcb5b58e1b40a948
jongwoo Yoo <dbwhddn10@gmail.com>
2025-07-22 21:40:25 +0900

    feat(mikro-orm): add QueryBuilder/getSingleResult example

---
+++ mikro-orm/QueryBuilder/getSingleResult/app.test.ts
@@ -0,0 +1,44 @@
+import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/core';
+import { EntityManager, SqliteDriver } from '@mikro-orm/sqlite';
+
+let orm: MikroORM;
+
+@Entity()
+export class User {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+beforeAll(async () => {
+  const config = {
+    entities: [User],
+    dbName: ':memory:',
+    driver: SqliteDriver,
+    allowGlobalContext: true,
+  } as Parameters<typeof MikroORM.init<SqliteDriver>>[0];
+  orm = await MikroORM.init(config);
+  await orm.getSchemaGenerator().createSchema();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('app', async () => {
+  const em = orm.em as EntityManager;
+
+  em.create(User, {
+    name: 'abcd',
+  });
+  em.create(User, {
+    name: 'bcde',
+  });
+
+  const result = await em.createQueryBuilder(User).getSingleResult();
+
+  expect(result).toBeInstanceOf(User);
+  expect(result?.name).toBe('abcd');
+});
