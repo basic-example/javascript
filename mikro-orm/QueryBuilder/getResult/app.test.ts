6b241dc3f18adaf497bbf5617b60e2c4ab89dc4b
jongwoo Yoo <dbwhddn10@gmail.com>
2025-07-22 20:54:06 +0900

    feat(mikro-orm): add QueryBuilder/getResult example

---
+++ mikro-orm/QueryBuilder/getResult/app.test.ts
@@ -0,0 +1,47 @@
+import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/core';
+import { EntityManager, SqliteDriver } from '@mikro-orm/sqlite';
+
+let orm: MikroORM;
+
+@Entity()
+export class User {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+beforeAll(async () => {
+  const config = {
+    entities: [User],
+    dbName: ':memory:',
+    driver: SqliteDriver,
+    allowGlobalContext: true,
+  } as Parameters<typeof MikroORM.init<SqliteDriver>>[0];
+  orm = await MikroORM.init(config);
+  await orm.getSchemaGenerator().createSchema();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('app', async () => {
+  const em = orm.em as EntityManager;
+
+  em.create(User, {
+    name: 'abcd',
+  });
+  em.create(User, {
+    name: 'bcde',
+  });
+
+  const result = await em.createQueryBuilder(User).getResult();
+
+  expect(result).toBeInstanceOf(Array);
+  expect(result[0]).toBeInstanceOf(User);
+  expect(result[1]).toBeInstanceOf(User);
+  expect(result[0].name).toBe('abcd');
+  expect(result[1].name).toBe('bcde');
+});
