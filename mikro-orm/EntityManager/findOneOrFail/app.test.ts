98615a2a01b88c177a8c26e58767f13d5ae7d9c4
jongwoo Yoo <dbwhddn10@gmail.com>
2025-07-24 06:22:56 +0900

    feat(mikro-orm): add EntityManager/findOneOrFail example

---
+++ mikro-orm/EntityManager/findOneOrFail/app.test.ts
@@ -0,0 +1,38 @@
+import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/core';
+import { EntityManager, SqliteDriver } from '@mikro-orm/sqlite';
+
+let orm: MikroORM;
+
+@Entity()
+export class User {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+beforeAll(async () => {
+  const config = {
+    entities: [User],
+    dbName: ':memory:',
+    driver: SqliteDriver,
+    allowGlobalContext: true,
+  } as Parameters<typeof MikroORM.init<SqliteDriver>>[0];
+  orm = await MikroORM.init(config);
+  await orm.getSchemaGenerator().createSchema();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('app', async () => {
+  const em = orm.em as EntityManager;
+  em.create(User, {
+    name: 'abcd',
+  });
+  await em.flush();
+
+  await expect(em.findOneOrFail(User, { id: 2 })).rejects.toThrow();
+});
