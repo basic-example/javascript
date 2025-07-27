1f21279c97c224f49609635341089394dd848cf1
jongwoo Yoo <dbwhddn10@gmail.com>
2025-07-27 10:33:56 +0900

    feat(mikro-orm): add QueryBuilder/count example

---
+++ mikro-orm/QueryBuilder/count/app.test.ts
@@ -0,0 +1,35 @@
+import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/core';
+import { EntityManager, SqliteDriver } from '@mikro-orm/sqlite';
+
+let orm: MikroORM;
+
+@Entity()
+export class User {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+beforeAll(async () => {
+  const config = {
+    entities: [User],
+    dbName: ':memory:',
+    driver: SqliteDriver,
+    allowGlobalContext: true,
+  } as Parameters<typeof MikroORM.init<SqliteDriver>>[0];
+  orm = await MikroORM.init(config);
+  await orm.getSchemaGenerator().createSchema();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('app', async () => {
+  const em = orm.em as EntityManager;
+  const qry = em.createQueryBuilder(User).count();
+
+  expect(qry.getQuery()).toBe('select count(*) as `count` from `user` as `u0`');
+});
