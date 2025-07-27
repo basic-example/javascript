1c5e1818802f7241aad4e76f175256a50941a587
jongwoo Yoo <dbwhddn10@gmail.com>
2025-07-27 20:37:33 +0900

    feat(mikro-orm): add QueryBuilder/orderBy example

---
+++ mikro-orm/QueryBuilder/orderBy/app.test.ts
@@ -0,0 +1,67 @@
+import {
+  Entity,
+  ManyToOne,
+  MikroORM,
+  PrimaryKey,
+  Property,
+} from '@mikro-orm/core';
+import { EntityManager, SqliteDriver } from '@mikro-orm/sqlite';
+
+let orm: MikroORM;
+
+@Entity()
+export class User {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+@Entity()
+export class Post {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  title!: string;
+
+  @Property()
+  category!: string;
+
+  @Property()
+  createdAt: Date = new Date();
+
+  @Property()
+  content!: string;
+
+  @ManyToOne(() => User)
+  author!: User;
+}
+
+beforeAll(async () => {
+  const config = {
+    entities: [User, Post],
+    dbName: ':memory:',
+    driver: SqliteDriver,
+    allowGlobalContext: true,
+  } as Parameters<typeof MikroORM.init<SqliteDriver>>[0];
+  orm = await MikroORM.init(config);
+  await orm.getSchemaGenerator().createSchema();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('app', async () => {
+  const em = orm.em as EntityManager;
+  const qry1 = em
+    .createQueryBuilder(Post)
+    .orderBy({ createdAt: 'desc', category: 'asc' });
+
+  await expect(qry1).resolves.not.toThrow();
+  expect(qry1.getQuery()).toBe(
+    'select `p0`.* from `post` as `p0` order by `p0`.`created_at` desc, `p0`.`category` asc',
+  );
+});
