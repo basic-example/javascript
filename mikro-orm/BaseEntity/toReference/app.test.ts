3fe7174834077ed80cb1a009a52caa637d2f027e
jongwoo Yoo <dbwhddn10@gmail.com>
2025-07-28 11:09:57 +0900

    feat(mikro-orm): add BaseEntity/toReference example

---
+++ mikro-orm/BaseEntity/toReference/app.test.ts
@@ -0,0 +1,46 @@
+import {
+  BaseEntity,
+  Entity,
+  MikroORM,
+  PrimaryKey,
+  Property,
+  Reference,
+} from '@mikro-orm/core';
+import { SqliteDriver } from '@mikro-orm/sqlite';
+
+let orm: MikroORM;
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+beforeAll(async () => {
+  const config = {
+    entities: [User],
+    dbName: ':memory:',
+    driver: SqliteDriver,
+    allowGlobalContext: true,
+  } as Parameters<typeof MikroORM.init<SqliteDriver>>[0];
+  orm = await MikroORM.init(config);
+  await orm.getSchemaGenerator().createSchema();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('app', async () => {
+  const user = new User();
+
+  user.assign({
+    name: 'abcd',
+  });
+
+  expect(user.toReference()).toBeInstanceOf(Reference);
+  expect(user.toReference().getEntity()).toEqual(user);
+});
