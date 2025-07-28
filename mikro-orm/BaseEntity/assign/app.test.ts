dd61d4ddf46b6e118e463775c195d3bba2fe3c37
jongwoo Yoo <dbwhddn10@gmail.com>
2025-07-28 10:49:43 +0900

    feat(mikro-orm): add BaseEntity/assign example

---
+++ mikro-orm/BaseEntity/assign/app.test.ts
@@ -0,0 +1,42 @@
+import {
+  BaseEntity,
+  Entity,
+  MikroORM,
+  PrimaryKey,
+  Property,
+} from '@mikro-orm/core';
+import { SqliteDriver } from '@mikro-orm/sqlite';
+
+let orm: MikroORM;
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+beforeAll(async () => {
+  const config = {
+    entities: [User],
+    dbName: ':memory:',
+    driver: SqliteDriver,
+    allowGlobalContext: true,
+  } as Parameters<typeof MikroORM.init<SqliteDriver>>[0];
+  orm = await MikroORM.init(config);
+  await orm.getSchemaGenerator().createSchema();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('app', async () => {
+  const user = new User();
+
+  user.assign({ name: 'abcd' });
+
+  expect(user.name).toBe('abcd');
+});
