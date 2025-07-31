28fe9bffc1f7b87adc72692d94075bfa25fa9350
jongwoo Yoo <dbwhddn10@gmail.com>
2025-07-31 15:15:19 +0900

    feat(mikro-orm): add QueryBuilder/toQuery example

---
+++ mikro-orm/QueryBuilder/toQuery/app.test.ts
@@ -0,0 +1,42 @@
+import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/core';
+import { EntityManager, SqliteDriver } from '@mikro-orm/sqlite';
+
+let orm: MikroORM;
+
+@Entity()
+export class User {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+}
+
+beforeAll(async () => {
+  const config = {
+    entities: [User],
+    dbName: ':memory:',
+    driver: SqliteDriver,
+    allowGlobalContext: true,
+  } as Parameters<typeof MikroORM.init<SqliteDriver>>[0];
+  orm = await MikroORM.init(config);
+  await orm.getSchemaGenerator().createSchema();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('app', async () => {
+  const em = orm.em as EntityManager;
+  const qry = em
+    .createQueryBuilder(User)
+    .select('*')
+    .where({ id: 1 })
+    .toQuery();
+
+  expect(qry.sql).toEqual(
+    'select `u0`.* from `user` as `u0` where `u0`.`id` = ?',
+  );
+  expect(qry.params).toEqual([1]);
+});
