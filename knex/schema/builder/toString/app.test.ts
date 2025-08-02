1a62e7db3b2c911bdc7db93ea03e898d63af71b6
jongwoo Yoo <dbwhddn10@gmail.com>
2025-08-02 13:53:52 +0900

    feat(knex): add schema/builder/toString example

---
+++ knex/schema/builder/toString/app.test.ts
@@ -0,0 +1,36 @@
+import knex, { Knex } from 'knex';
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: ':memory:',
+  },
+  useNullAsDefault: true,
+});
+
+beforeAll(async () => {
+  await db.schema.createTable('users', (table: Knex.CreateTableBuilder) => {
+    table.increments('id').primary();
+    table.string('name');
+    table.integer('age');
+  });
+
+  await db<{
+    id: number;
+    name: string;
+    age: number;
+  }>('users').insert([
+    { name: 'Alice', age: 30 },
+    { name: 'Bob', age: 25 },
+  ]);
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+test('app', async () => {
+  const sql = db.select('*').from('users').where('id', 1).toString();
+
+  expect(sql).toBe('select * from `users` where `id` = 1');
+});
