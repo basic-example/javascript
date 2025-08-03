8c6d8abdbb71c08b7488965374cb07c01c4ecaa5
jongwoo Yoo <dbwhddn10@gmail.com>
2025-08-03 13:26:40 +0900

    feat(objection): add QueryBuilder/insertAndFetch example

---
+++ objection/QueryBuilder/insertAndFetch/app.test.ts
@@ -0,0 +1,61 @@
+import Knex from 'knex';
+import { knexSnakeCaseMappers, Model } from 'objection';
+
+let knex: Knex.Knex;
+
+class User extends Model {
+  id!: number;
+  name!: string;
+  age!: number;
+
+  static tableName = 'users';
+  static jsonSchema = {
+    type: 'object',
+    required: ['name', 'age'],
+    properties: {
+      id: { type: 'integer' },
+      name: { type: 'string', minLength: 1, maxLength: 255 },
+      age: { type: 'integer', minimum: 1 },
+    },
+  };
+}
+
+beforeAll(async () => {
+  knex = Knex({
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:',
+    },
+    useNullAsDefault: true,
+    ...knexSnakeCaseMappers(),
+  });
+  Model.knex(knex);
+
+  await knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.integer('age');
+  });
+});
+
+afterAll(async () => {
+  await knex.destroy();
+});
+
+test('app', async () => {
+  const querySpy = jest.spyOn(knex.client, 'query');
+
+  await User.query().insertAndFetch({ name: 'bar', age: 27 });
+
+  const sql1 = (querySpy.mock.calls[0][1] as Knex.Knex.Sql).toNative();
+  const sql2 = (querySpy.mock.calls[1][1] as Knex.Knex.Sql).toNative();
+
+  expect(querySpy).toHaveBeenCalled();
+  expect(querySpy.mock.calls.length).toEqual(2);
+  expect(sql1.sql).toEqual('insert into `users` (`age`, `name`) values (?, ?)');
+  expect(sql1.bindings).toEqual([27, 'bar']);
+  expect(sql2.sql).toEqual(
+    'select `users`.* from `users` where `users`.`id` in (?)',
+  );
+  expect(sql2.bindings).toEqual([1]);
+});
