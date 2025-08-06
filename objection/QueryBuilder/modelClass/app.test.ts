c213d420cef76fd104bd64b025d58f8b289f01cb
jongwoo Yoo <dbwhddn10@gmail.com>
2025-08-07 04:03:12 +0900

    feat(objection): add QueryBuilder/modelClass example

---
+++ objection/QueryBuilder/modelClass/app.test.ts
@@ -0,0 +1,46 @@
+import Knex from 'knex';
+import { knexSnakeCaseMappers, Model } from 'objection';
+
+let knex: Knex.Knex;
+
+class User extends Model {
+  id!: number;
+  name!: string;
+
+  static tableName = 'users';
+  static jsonSchema = {
+    type: 'object',
+    required: ['name'],
+    properties: {
+      id: { type: 'integer' },
+      name: { type: 'string', minLength: 1, maxLength: 255 },
+    },
+  };
+}
+
+beforeAll(async () => {
+  knex = Knex({
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:',
+    },
+    useNullAsDefault: true,
+    ...knexSnakeCaseMappers(),
+  });
+  Model.knex(knex);
+
+  await knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+  });
+});
+
+afterAll(async () => {
+  await knex.destroy();
+});
+
+test('app', async () => {
+  const model = User.query().modelClass();
+
+  expect(model).toBe(User);
+});
